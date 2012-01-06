26c10639c702ffd62af3b87c2643b423428c2c83
Frankie Wu <qmwu2000@gmail.com>
2012-01-06 16:35:23 +0800

    add AllTest.java

---
+++ cat-core/src/test/java/com/dianping/cat/message/AllTests.java
@@ -0,0 +1,40 @@
+package com.dianping.cat.message;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.dianping.cat.message.configuration.model.ConfigTest;
+import com.dianping.cat.message.internal.MessageProducerTest;
+import com.dianping.cat.message.io.InMemoryTest;
+import com.dianping.cat.message.io.TcpSocketTest;
+import com.dianping.cat.message.spi.codec.PlainTextMessageCodecTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+
+/* <default> */
+EventTest.class,
+
+HeartbeatTest.class,
+
+TransactionTest.class,
+
+/* .configuration.model */
+ConfigTest.class,
+
+/* .internal */
+MessageProducerTest.class,
+
+/* .io */
+InMemoryTest.class,
+
+TcpSocketTest.class,
+
+/* .spi.codec */
+PlainTextMessageCodecTest.class,
+
+})
+public class AllTests {
+
+}
