1a506927c588c92054e12aec29f97b234c77a461
youyong <yong.you@dianping.com>
2012-01-09 15:14:34 +0800

    modify the test case config path

---
+++ cat-consumer/src/test/java/com/dianping/cat/message/consumer/model/failure/FailureAnaylzerStoreTestConfigurator.java
@@ -0,0 +1,35 @@
+package com.dianping.cat.message.consumer.model.failure;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dianping.cat.message.consumer.model.failure.FailureReportAnalyzer.Handler;
+import com.site.lookup.configuration.AbstractResourceConfigurator;
+import com.site.lookup.configuration.Component;
+
+public class FailureAnaylzerStoreTestConfigurator extends
+		AbstractResourceConfigurator {
+	public static void main(String[] args) {
+		generatePlexusComponentsXmlFile(new FailureAnaylzerStoreTestConfigurator());
+	}
+
+	@Override
+	public List<Component> defineComponents() {
+		List<Component> all = new ArrayList<Component>();
+		all.add(C(FailureReportAnalyzer.class) //
+				.config(E("reportPath").value("./target/report/failure/"))
+				.is(PER_LOOKUP)//
+				.req(Handler.class, new String[] { "failure", "long-url" },
+						"m_handlers"));
+		
+		return all;
+	}
+
+	@Override
+	protected File getConfigurationFile() {
+		return new File("src/test/resources/"
+				+ FailureAnalyzerStoreTest.class.getName().replace('.', '/')
+				+ ".xml");
+	}
+}
