f25d197217b1553f81bf94e9709b6e64a13535e1
youyong <yong.you@dianping.com>
2012-01-10 11:40:47 +0800

    add json test

--- cat-consumer/src/test/java/com/dianping/cat/message/consumer/model/failure/FailureAnalyzerStoreTest.java
+++ cat-consumer/src/test/java/com/dianping/cat/message/consumer/model/failure/FailureAnalyzerStoreTest.java
@@ -9,16 +9,58 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import com.dianping.cat.consumer.model.failure.entity.FailureReport;
+import com.dianping.cat.consumer.model.failure.transform.DefaultJsonBuilder;
 import com.dianping.cat.message.consumer.impl.AnalyzerFactory;
 import com.dianping.cat.message.internal.DefaultTransaction;
 import com.dianping.cat.message.spi.MessageTree;
 import com.dianping.cat.message.spi.internal.DefaultMessageTree;
+import com.google.gson.Gson;
 import com.site.helper.Files;
 import com.site.lookup.ComponentTestCase;
 
 @RunWith(JUnit4.class)
 public class FailureAnalyzerStoreTest extends ComponentTestCase {
 
+	@Test 
+	public void testJson()throws Exception{
+		long current = System.currentTimeMillis();
+		long duration = 60 * 60 * 1000;
+		long extraTime = 5 * 60 * 1000;
+		long start = current - current % (60 * 60 * 1000);
+
+		AnalyzerFactory factory = lookup(AnalyzerFactory.class);
+		FailureReportAnalyzer analyzer = (FailureReportAnalyzer) factory
+				.create("failure", start, duration, "domain1", extraTime);
+		int number = 4;
+		for (int i = 0; i < number; i++) {
+			DefaultTransaction t = new DefaultTransaction("A1", "B1", null);
+			MessageTree tree = new DefaultMessageTree();
+			tree.setMessageId("thread0001");
+			tree.setDomain("middleware");
+			tree.setHostName("middleware");
+			tree.setMessage(t);
+			tree.setIpAddress("192.168.8.1");
+			t.setDuration(3 * 1000);
+			t.setTimestamp(start + 1000 * 60 * i);
+			analyzer.process(tree);
+			analyzer.process(tree);
+			analyzer.process(tree);
+		}
+
+		FailureReport report = analyzer.generate();
+		analyzer.store(report);
+
+		DefaultJsonBuilder jsonBuilder = new DefaultJsonBuilder();
+		jsonBuilder.visitFailureReport(report);
+		String realResult = jsonBuilder.getString();
+
+		Gson gson = new Gson();
+		String exceptedResult = gson.toJson(report,FailureReport.class);
+		
+		Assert.assertEquals("Check json content!", exceptedResult, realResult);
+	}
+	
+	
 	@Test
 	public void testStore() throws Exception {
 		long current = System.currentTimeMillis();
@@ -29,26 +71,28 @@ public class FailureAnalyzerStoreTest extends ComponentTestCase {
 		AnalyzerFactory factory = lookup(AnalyzerFactory.class);
 		FailureReportAnalyzer analyzer = (FailureReportAnalyzer) factory
 				.create("failure", start, duration, "domain1", extraTime);
-
-		DefaultTransaction t = new DefaultTransaction("A1", "B1", null);
-		MessageTree tree = new DefaultMessageTree();
-		tree.setMessageId("thread0001");
-		tree.setDomain("middleware");
-		tree.setHostName("middleware");
-		tree.setMessage(t);
-		t.setDuration(3 * 1000);
-		t.setTimestamp(start + 1000 * 60);
-		analyzer.process(tree);
+		int number = 20;
+		for (int i = 0; i < number; i++) {
+			DefaultTransaction t = new DefaultTransaction("A1", "B1", null);
+			MessageTree tree = new DefaultMessageTree();
+			tree.setMessageId("thread0001");
+			tree.setDomain("middleware");
+			tree.setHostName("middleware");
+			tree.setMessage(t);
+			t.setDuration(3 * 1000);
+			t.setTimestamp(start + 1000 * 60 * i);
+			analyzer.process(tree);
+		}
 
 		FailureReport report = analyzer.generate();
 		analyzer.store(report);
-		
+
 		String parentPath = analyzer.getReportPath();
-		String pathname = parentPath+analyzer.getFailureFileName(report);
-		File storeFile = new File(pathname+".html");
-		Assert.assertEquals("Check file is exist!",true, storeFile.exists());
-		String realResult=Files.forIO().readFrom(storeFile, "utf-8");
+		String pathname = parentPath + analyzer.getFailureFileName(report);
+		File storeFile = new File(pathname + ".html");
+		Assert.assertEquals("Check file is exist!", true, storeFile.exists());
+		String realResult = Files.forIO().readFrom(storeFile, "utf-8");
 		String exceptedResult = FailureReportStore.getStoreString(report);
-		Assert.assertEquals("Check file content!", exceptedResult ,realResult);
+		Assert.assertEquals("Check file content!", exceptedResult, realResult);
 	}
 }
