f21ceabd6e014ed5b782123034d72f9bb6a7a4dc
Frankie Wu <qmwu2000@gmail.com>
2012-02-03 14:29:16 +0800

    adding IpAnalyzer

--- cat-consumer/src/main/java/com/dianping/cat/consumer/ip/IpAnalyzer.java
+++ cat-consumer/src/main/java/com/dianping/cat/consumer/ip/IpAnalyzer.java
@@ -1,41 +1,102 @@
 package com.dianping.cat.consumer.ip;
 
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.dianping.cat.consumer.ip.model.entity.Ip;
 import com.dianping.cat.consumer.ip.model.entity.IpReport;
+import com.dianping.cat.consumer.ip.model.entity.Segment;
+import com.dianping.cat.message.Message;
+import com.dianping.cat.message.Transaction;
 import com.dianping.cat.message.spi.AbstractMessageAnalyzer;
 import com.dianping.cat.message.spi.MessageTree;
 
 public class IpAnalyzer extends AbstractMessageAnalyzer<IpReport> {
+	private Map<String, IpReport> m_reports = new HashMap<String, IpReport>();
+
+	private IpReport findOrCreateReport(String domain) {
+		IpReport report = m_reports.get(domain);
+
+		if (report == null) {
+			synchronized (m_reports) {
+				report = m_reports.get(domain);
+
+				if (report == null) {
+					report = new IpReport().setDomain(domain);
+					m_reports.put(domain, report);
+				}
+			}
+		}
+
+		return report;
+	}
 
 	@Override
-   protected void store(List<IpReport> result) {
-	   // TODO Auto-generated method stub
-	   
-   }
+	public List<IpReport> generate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
-   public List<IpReport> generate() {
-	   // TODO Auto-generated method stub
-	   return null;
-   }
+	public IpReport generate(String domain) {
+		IpReport report = null;
+
+		if (domain == null) {
+			if (!m_reports.isEmpty()) {
+				domain = m_reports.keySet().iterator().next();
+			}
+		}
+
+		if (domain != null) {
+			report = m_reports.get(domain);
+		}
+
+		return report;
+	}
+
+	private String getIpAddress(Transaction root) {
+		List<Message> children = ((Transaction) root).getChildren();
+
+		return null;
+	}
 
 	@Override
-   public IpReport generate(String domain) {
-	   // TODO Auto-generated method stub
-	   return null;
-   }
+	protected boolean isTimeout() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	@Override
-   protected void process(MessageTree tree) {
-	   // TODO Auto-generated method stub
-	   
-   }
+	protected void process(MessageTree tree) {
+		Message root = tree.getMessage();
+
+		if (root instanceof Transaction) {
+			String address = getIpAddress((Transaction) root);
+
+			if (address == null) {
+				address = "N/A";
+			}
+
+			String domain = tree.getDomain();
+			Calendar cal = Calendar.getInstance();
+
+			cal.setTimeInMillis(root.getTimestamp());
+
+			int minute = cal.get(Calendar.MINUTE);
+			IpReport report = findOrCreateReport(domain);
+			Segment segment = report.findOrCreateSegment(minute);
+			Ip ip = segment.findOrCreateIp(address);
+
+			ip.setCount(ip.getCount() + 1);
+		}
+	}
 
 	@Override
-   protected boolean isTimeout() {
-	   // TODO Auto-generated method stub
-	   return false;
-   }
-	
+	protected void store(List<IpReport> reports) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
