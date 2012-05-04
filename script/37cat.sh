0c54af1d657f92b128ea625d9317842131c9b37d
You Yong <yong.you@dianping.com>
2012-05-04 10:46:37 +0800

    add script

---
+++ script/37cat.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/expect 
+set timeout 30
+spawn ssh -p 58422 root@10.1.6.37  killall -9 java && cd /usr/local/tomcat/webapps && rm cat* -rf && cd /usr/local/tomcat/work && rm * -rf
+expect "*?assword:*"
+send "12qwaszx\r"
+
+set timeout 30
+spawn scp -P58422 /home/youyong/workspace/cat/cat-home/target/cat.war root@10.1.6.37:/usr/local/tomcat/webapps
+expect "*?assword:*"
+send "12qwaszx\r"
+interact
+
+set timeout 30
+spawn ssh -p 58422 root@10.1.6.37  cd /usr/local/tomcat/bin && ./startup.sh
+expect "*?assword:*"
+send "12qwaszx\r"
+interact 
