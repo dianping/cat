116b123ec58dafcbea927e480a924364131ea92e
sean.wang <xalinx@gmail.com>
2012-06-14 13:46:11 +0800

    problem graph

--- cat-home/src/main/java/com/dianping/cat/report/task/DefaultTaskConsumer.java
+++ cat-home/src/main/java/com/dianping/cat/report/task/DefaultTaskConsumer.java
@@ -13,6 +13,7 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.locks.LockSupport;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.plexus.logging.LogEnabled;
 import org.codehaus.plexus.logging.Logger;
 import org.xml.sax.SAXException;
@@ -22,7 +23,9 @@ import com.dianping.cat.consumer.event.model.entity.EventName;
 import com.dianping.cat.consumer.event.model.entity.EventReport;
 import com.dianping.cat.consumer.event.model.entity.EventType;
 import com.dianping.cat.consumer.heartbeat.model.entity.HeartbeatReport;
+import com.dianping.cat.consumer.problem.model.entity.JavaThread;
 import com.dianping.cat.consumer.problem.model.entity.ProblemReport;
+import com.dianping.cat.consumer.problem.model.entity.Segment;
 import com.dianping.cat.consumer.transaction.model.entity.Machine;
 import com.dianping.cat.consumer.transaction.model.entity.TransactionName;
 import com.dianping.cat.consumer.transaction.model.entity.TransactionReport;
@@ -44,6 +47,7 @@ import com.dianping.cat.report.page.model.heartbeat.HeartbeatReportMerger;
 import com.dianping.cat.report.page.model.problem.ProblemReportMerger;
 import com.dianping.cat.report.page.model.transaction.TransactionReportMerger;
 import com.site.dal.jdbc.DalException;
+import com.site.helper.Joiners;
 import com.site.lookup.annotation.Inject;
 
 /**
@@ -64,6 +68,8 @@ public class DefaultTaskConsumer extends TaskConsumer implements LogEnabled {
 		double sum;
 
 		double sum2;
+
+		public int[] minuteCounts;
 	}
 
 	@Inject
@@ -404,9 +410,136 @@ public class DefaultTaskConsumer extends TaskConsumer implements LogEnabled {
 		return null;
 	}
 
-	private List<Graph> splitProblemReportToGraphs(Date reportPeroid, String reportDomain, String reportName, ProblemReport problemReport) {
-		// TODO Auto-generated method stub
-		return null;
+	private List<Graph> splitProblemReportToGraphs(Date reportPeroid, String domainName, String reportName, ProblemReport problemReport) {
+		Set<String> ips = problemReport.getIps();
+		List<Graph> graphs = new ArrayList<Graph>(ips.size() + 1); // all and every machine
+		Map<String, GraphLine> allDetailCache = new TreeMap<String, GraphLine>();
+		Map<String, GraphLine> allSummaryCache = new TreeMap<String, GraphLine>();
+
+		for (String ip : ips) {
+			Map<String, GraphLine> detailCache = new TreeMap<String, GraphLine>();
+			Map<String, GraphLine> summaryCache = new TreeMap<String, GraphLine>();
+			Graph graph = new Graph();
+			graph.setIp(ip);
+			graph.setDomain(domainName);
+			graph.setName(reportName);
+			graph.setPeriod(reportPeroid);
+			graph.setType(3);
+			com.dianping.cat.consumer.problem.model.entity.Machine machine = problemReport.getMachines().get(ip);
+			Map<String, JavaThread> types = machine.getThreads();
+
+			for (Entry<String, JavaThread> transactionEntry : types.entrySet()) {
+				JavaThread thread = transactionEntry.getValue();
+				for (Entry<Integer, Segment> segmentEntry : thread.getSegments().entrySet()) {
+					Segment segment = segmentEntry.getValue();
+					int minute = segment.getId();
+					for (com.dianping.cat.consumer.problem.model.entity.Entry entry : segment.getEntries()) {
+						String summaryKey = entry.getType();
+						GraphLine summaryLine = summaryCache.get(summaryKey);
+						if (summaryLine == null) {
+							summaryLine = new GraphLine();
+							summaryLine.minuteCounts = new int[60];
+							summaryCache.put(summaryKey, summaryLine);
+						}
+						summaryLine.totalCount++;
+						summaryLine.minuteCounts[minute]++;
+
+						GraphLine allSummaryLine = allSummaryCache.get(summaryKey);
+						if (allSummaryLine == null) {
+							allSummaryLine = new GraphLine();
+							allSummaryLine.minuteCounts = new int[60];
+							allSummaryCache.put(summaryKey, allSummaryLine);
+						}
+						allSummaryLine.totalCount++;
+						allSummaryLine.minuteCounts[minute]++;
+
+						String detailKey = entry.getType() + "\t" + entry.getStatus();
+						GraphLine detailLine = detailCache.get(detailKey);
+						if (detailLine == null) {
+							detailLine = new GraphLine();
+							detailLine.minuteCounts = new int[60];
+							detailCache.put(detailKey, detailLine);
+						}
+						detailLine.totalCount++;
+						detailLine.minuteCounts[minute]++;
+
+						GraphLine allDetailLine = allDetailCache.get(detailKey);
+						if (allDetailLine == null) {
+							allDetailLine = new GraphLine();
+							allDetailLine.minuteCounts = new int[60];
+							allDetailCache.put(detailKey, allDetailLine);
+						}
+						allDetailLine.totalCount++;
+						allDetailLine.minuteCounts[minute]++;
+					}
+				}
+
+			}
+
+			StringBuilder summaryBuilder = new StringBuilder();
+			for (Entry<String, GraphLine> summaryEntry : summaryCache.entrySet()) {
+				GraphLine summaryLine = summaryEntry.getValue();
+				summaryBuilder.append(summaryEntry.getKey());
+				summaryBuilder.append("\t");
+				summaryBuilder.append(summaryLine.totalCount);
+				summaryBuilder.append("\t");
+				summaryBuilder.append(TaskHelper.join(summaryLine.minuteCounts, ','));
+				summaryBuilder.append("\n");
+			}
+			graph.setSummaryContent(summaryBuilder.toString());
+
+			StringBuilder detailBuilder = new StringBuilder();
+			for (Entry<String, GraphLine> detailEntry : detailCache.entrySet()) {
+				GraphLine detailLine = detailEntry.getValue();
+				detailBuilder.append(detailEntry.getKey());
+				detailBuilder.append("\t");
+				detailBuilder.append(detailLine.totalCount);
+				detailBuilder.append("\t");
+				detailBuilder.append(TaskHelper.join(detailLine.minuteCounts, ','));
+				detailBuilder.append("\n");
+			}
+			graph.setDetailContent(detailBuilder.toString());
+
+			graph.setCreationDate(new Date());
+			graphs.add(graph);
+		}
+
+		Graph allGraph = new Graph();
+		allGraph.setIp("all");
+		allGraph.setDomain(domainName);
+		allGraph.setName(reportName);
+		allGraph.setPeriod(reportPeroid);
+		allGraph.setType(3);
+
+		StringBuilder summaryBuilder = new StringBuilder();
+		for (Entry<String, GraphLine> summaryEntry : allSummaryCache.entrySet()) {
+			GraphLine summaryLine = summaryEntry.getValue();
+			summaryBuilder.append(summaryEntry.getKey());
+			summaryBuilder.append("\t");
+			summaryBuilder.append(summaryLine.totalCount);
+			summaryBuilder.append("\t");
+			summaryBuilder.append(TaskHelper.join(summaryLine.minuteCounts, ','));
+			summaryBuilder.append("\n");
+		}
+		allGraph.setSummaryContent(summaryBuilder.toString());
+
+		StringBuilder detailBuilder = new StringBuilder();
+		for (Entry<String, GraphLine> detailEntry : allDetailCache.entrySet()) {
+			GraphLine detailLine = detailEntry.getValue();
+			detailBuilder.append(detailEntry.getKey());
+			detailBuilder.append("\t");
+			detailBuilder.append(detailLine.totalCount);
+			detailBuilder.append("\t");
+			detailBuilder.append(TaskHelper.join(detailLine.minuteCounts, ','));
+			detailBuilder.append("\n");
+		}
+		allGraph.setDetailContent(detailBuilder.toString());
+
+		allGraph.setCreationDate(new Date());
+
+		graphs.add(allGraph);
+
+		return graphs;
 	}
 
 	private List<Graph> splitTransactionReportToGraphs(Date reportPeroid, String domainName, String reportName, TransactionReport transactionReport) {
