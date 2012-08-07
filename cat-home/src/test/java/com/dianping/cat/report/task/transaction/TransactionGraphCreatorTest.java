1a2d2dceaa43e80b077b460a3b9e370f23fe3dbe
youyong <youyong205@126.com>
2012-08-07 16:02:13 +0800

    modify the cat event graph

--- cat-home/src/test/java/com/dianping/cat/report/task/transaction/TransactionGraphCreatorTest.java
+++ cat-home/src/test/java/com/dianping/cat/report/task/transaction/TransactionGraphCreatorTest.java
@@ -20,14 +20,57 @@ import com.dianping.cat.report.task.transaction.TransactionGraphCreator;
 public class TransactionGraphCreatorTest {
 
 	@Test
-	public void test() throws Exception {
+	public void testSplitReportToGraphs() throws Exception {
 		TransactionGraphCreator creator = new TransactionGraphCreator();
-		String xml = Files.forIO().readFrom(getClass().getResourceAsStream("transactionCreator.xml"), "utf-8");
+		String xml = Files.forIO().readFrom(getClass().getResourceAsStream("BaseTransactionReportForGraph.xml"), "utf-8");
 		TransactionReport report = new DefaultDomParser().parse(xml);
 		Date date = new Date();
 		List<Graph> graphs = creator.splitReportToGraphs(date, "MobileApi", "transaction", report);
+		Map<String, Range> realResult = new HashMap<String, Range>();
+		Map<String, Range> excepectedResult = buildExcepetedResult();
+		buildRealResult(graphs, realResult);
+
+		Assert.assertEquals(realResult.size(), excepectedResult.size());
+		for (String str : realResult.keySet()) {
+			Range realRange = realResult.get(str);
+			Range exceptedRange = excepectedResult.get(str);
+			
+			assertStr(realRange.total, exceptedRange.total);
+			assertStr(realRange.fail, exceptedRange.fail);
+			assertStr(realRange.sum, exceptedRange.sum);
+		}
+	}
+
+	private void assertStr(String expected, String real) {
+		String[] expecteds = expected.split(",");
+		String[] reals = real.split(",");
+
+		Assert.assertEquals(expecteds.length, reals.length);
+		for (int i = 0; i < expecteds.length; i++) {
+			Assert.assertEquals(Double.parseDouble(expecteds[i]), Double.parseDouble(reals[i]));
+		}
+	}
+
+	private Map<String, Range> buildExcepetedResult() throws Exception {
 		Map<String, Range> result = new HashMap<String, Range>();
-		Map<String, Range> result2 = new HashMap<String, Range>();
+		String contents = Files.forIO().readFrom(getClass().getResourceAsStream("TransactionGraphResult"), "utf-8");
+		String[] lines = contents.split("\n");
+
+		for (String line : lines) {
+			String[] tabs = line.split("\t");
+			if (tabs.length > 3) {
+				Range range = new Range();
+				range.total = tabs[1];
+				range.sum = tabs[2];
+				range.fail = tabs[3];
+
+				result.put(tabs[0], range);
+			}
+		}
+		return result;
+	}
+
+	private void buildRealResult(List<Graph> graphs, Map<String, Range> realResult) {
 		for (Graph graph : graphs) {
 			String ip = graph.getIp();
 			String summaryContent = graph.getSummaryContent();
@@ -36,10 +79,10 @@ public class TransactionGraphCreatorTest {
 				String records[] = line.split("\t");
 				String type = records[0];
 				Range range = new Range();
-				range.total = getSum(records[SummaryOrder.TOTAL_COUNT.ordinal()]);
-				range.fail = getSum(records[SummaryOrder.FAILURE_COUNT.ordinal()]);
-				range.sum = getSum(records[SummaryOrder.SUM.ordinal()]);
-				result.put(ip + ',' + type, range);
+				range.total = records[SummaryOrder.TOTAL_COUNT.ordinal()];
+				range.fail = records[SummaryOrder.FAILURE_COUNT.ordinal()];
+				range.sum = records[SummaryOrder.SUM.ordinal()];
+				realResult.put(ip + ':' + type, range);
 			}
 			String detailContent = graph.getDetailContent();
 			lines = detailContent.split("\n");
@@ -48,66 +91,19 @@ public class TransactionGraphCreatorTest {
 				String type = records[0];
 				String name = records[1];
 				Range range = new Range();
-				range.total = getSum(records[DetailOrder.TOTAL_COUNT.ordinal()]);
-				range.fail = getSum(records[DetailOrder.FAILURE_COUNT.ordinal()]);
-				range.sum = getSum(records[DetailOrder.SUM.ordinal()]);
-				result.put(ip + ',' + type + ',' + name, range);
+				range.total = records[DetailOrder.TOTAL_COUNT.ordinal()];
+				range.fail = records[DetailOrder.FAILURE_COUNT.ordinal()];
+				range.sum = records[DetailOrder.SUM.ordinal()];
+				realResult.put(ip + ':' + type + ':' + name, range);
 			}
 		}
-
-		TransactionHelpGraphCreator creator2 = new TransactionHelpGraphCreator();
-		List<Graph> graphs2 = creator2.splitReportToGraphs(date, "MobileApi", "transaction", report);
-		for (Graph graph : graphs2) {
-			String ip = graph.getIp();
-			String summaryContent = graph.getSummaryContent();
-			String lines[] = summaryContent.split("\n");
-			for (String line : lines) {
-				String records[] = line.split("\t");
-				String type = records[0];
-				Range range = new Range();
-				range.total = getSum(records[SummaryOrder.TOTAL_COUNT.ordinal()]);
-				range.fail = getSum(records[SummaryOrder.FAILURE_COUNT.ordinal()]);
-				range.sum = getSum(records[SummaryOrder.SUM.ordinal()]);
-				result2.put(ip + ',' + type, range);
-			}
-			String detailContent = graph.getDetailContent();
-			lines = detailContent.split("\n");
-			for (String line : lines) {
-				String records[] = line.split("\t");
-				String type = records[0];
-				String name = records[1];
-				Range range = new Range();
-				range.total = getSum(records[DetailOrder.TOTAL_COUNT.ordinal()]);
-				range.fail = getSum(records[DetailOrder.FAILURE_COUNT.ordinal()]);
-				range.sum = getSum(records[DetailOrder.SUM.ordinal()]);
-				result2.put(ip + ',' + type + ',' + name, range);
-			}
-		}
-
-		Assert.assertEquals(result.size(), result2.size());
-		for (String str : result.keySet()) {
-			Range range1 = result.get(str);
-			Range range2 = result2.get(str);
-			Assert.assertEquals(true, range1.fail - range2.fail < 0.1);
-			Assert.assertEquals(true, range1.total - range2.total < 0.1);
-			Assert.assertEquals(true, range1.sum - range2.sum < 0.1);
-		}
-	}
-
-	private double getSum(String str) {
-		String[] strs = str.split(",");
-		double sum = 0;
-		for (String temp : strs) {
-			sum = sum + Double.parseDouble(temp);
-		}
-		return sum;
 	}
 
 	static class Range {
-		double total;
+		String total;
 
-		double fail;
+		String fail;
 
-		double sum;
+		String sum;
 	}
 }
