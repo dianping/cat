f0b27195414b37895138e3e0461a5b6e2f983505
Frankie Wu <qmwu2000@gmail.com>
2012-08-23 18:11:09 +0800

    slim log4j

--- bee-engine/src/test/java/com/dianping/bee/db/CatDatabase.java
+++ bee-engine/src/test/java/com/dianping/bee/db/CatDatabase.java
@@ -40,7 +40,17 @@ public class CatDatabase implements DatabaseProvider {
 			}
 		},
 
-		Event("event"),
+		Event("event") {
+			@Override
+			public TransactionColumn[] getColumns() {
+				return TransactionColumn.values();
+			}
+
+			@Override
+			public TransactionIndex[] getIndexes() {
+				return TransactionIndex.values();
+			}
+		},
 
 		Heartbeat("heartbeat"),
 
@@ -96,8 +106,6 @@ public class CatDatabase implements DatabaseProvider {
 	}
 
 	public static enum TransactionColumn implements ColumnMeta {
-		Id(String.class), // hash, auto generated
-
 		StartTime(String.class), // 20120822(for daily), 2012082213(for hour)
 
 		Domain(String.class), // MobileApi
@@ -138,8 +146,7 @@ public class CatDatabase implements DatabaseProvider {
 				}
 			}
 
-			throw new RuntimeException(String.format("Column(%s) is not found in %s", name,
-			      TransactionColumn.class.getName()));
+			throw new RuntimeException(String.format("Column(%s) is not found in %s", name, TransactionColumn.class.getName()));
 		}
 
 		@Override
@@ -164,8 +171,109 @@ public class CatDatabase implements DatabaseProvider {
 			int length = args.length;
 
 			if (length % 2 != 0) {
-				throw new IllegalArgumentException(String.format("Parameters should be paired for %s(%s)!", getClass(),
-				      name()));
+				throw new IllegalArgumentException(String.format("Parameters should be paired for %s(%s)!", getClass(), name()));
+			}
+
+			m_columns = new ColumnMeta[length / 2];
+			m_orders = new boolean[length / 2];
+
+			for (int i = 0; i < length / 2; i++) {
+				m_columns[i] = (ColumnMeta) args[2 * i];
+				m_orders[i] = (Boolean) args[2 * i + 1];
+			}
+		}
+
+		@Override
+		public ColumnMeta getColumn(int index) {
+			if (index >= 0 && index < m_columns.length) {
+				return m_columns[index];
+			} else {
+				throw new IndexOutOfBoundsException("size: " + m_columns.length + ", index: " + index);
+			}
+		}
+
+		@Override
+		public int getLength() {
+			return m_columns.length;
+		}
+
+		@Override
+		public boolean isAscend(int index) {
+			if (index >= 0 && index < m_orders.length) {
+				return m_orders[index];
+			} else {
+				throw new IndexOutOfBoundsException("size: " + m_orders.length + ", index: " + index);
+			}
+		}
+	}
+
+	public static enum EventColumn implements ColumnMeta {
+		StartTime(String.class), // 20120822(for daily), 2012082213(for hour)
+
+		Domain(String.class), // MobileApi
+
+		Type(String.class), // URL
+
+		Name(String.class), // /deallist.bin
+
+		TotalCount(Integer.class), // 2033
+
+		Failures(Integer.class), // 5
+
+		SampleMessage(String.class), // MobileApi-0a0101a6-1345600834200-1
+
+		MinDuration(Integer.class), // 1
+
+		MaxDuration(Integer.class), // 1234
+
+		SumDuration(Long.class), // 123456
+
+		Sum2Duration(Long.class), // 2364233
+
+		Line95(Integer.class); // 123
+
+		private String m_name;
+
+		private Class<?> m_type;
+
+		private EventColumn(Class<?> type) {
+			m_type = type;
+			m_name = name().toLowerCase();
+		}
+
+		public static EventColumn findByName(String name) {
+			for (EventColumn column : values()) {
+				if (column.getName().equalsIgnoreCase(name)) {
+					return column;
+				}
+			}
+
+			throw new RuntimeException(String.format("Column(%s) is not found in %s", name, EventColumn.class.getName()));
+		}
+
+		@Override
+		public String getName() {
+			return m_name;
+		}
+
+		@Override
+		public Class<?> getType() {
+			return m_type;
+		}
+	}
+
+	public static enum EventIndex implements Index {
+		IDX_STARTTIME_DOMAIN(EventColumn.StartTime, false, EventColumn.Domain, true);
+
+		private ColumnMeta[] m_columns;
+
+		private boolean[] m_orders;
+
+		private EventIndex(Object... args) {
+			int length = args.length;
+
+			if (length % 2 != 0) {
+				throw new IllegalArgumentException(String.format("Parameters should be paired for %s(%s)!", getClass(), name()));
 			}
 
 			m_columns = new ColumnMeta[length / 2];
