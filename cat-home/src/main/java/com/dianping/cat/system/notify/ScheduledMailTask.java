58219d3b42506d08f82330373b3c406be75828c7
youyong <youyong205@126.com>
2012-11-12 15:38:25 +0800

    modify the task time

--- cat-home/src/main/java/com/dianping/cat/system/notify/ScheduledMailTask.java
+++ cat-home/src/main/java/com/dianping/cat/system/notify/ScheduledMailTask.java
@@ -53,7 +53,7 @@ public class ScheduledMailTask implements Task {
 		Calendar cal = Calendar.getInstance();
 
 		cal.add(Calendar.DAY_OF_MONTH, 1);
-		cal.set(Calendar.HOUR, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 1);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.SECOND, 0);
 		cal.set(Calendar.MILLISECOND, 0);
