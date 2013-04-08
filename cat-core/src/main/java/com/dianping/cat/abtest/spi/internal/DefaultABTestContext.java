5fc38a2e9ddbb8c84deab149b069ed8903012492
Frankie Wu <qmwu2000@gmail.com>
2013-04-08 15:24:16 +0800

    integrate group strategy into context manager

--- cat-core/src/main/java/com/dianping/cat/abtest/spi/internal/DefaultABTestContext.java
+++ cat-core/src/main/java/com/dianping/cat/abtest/spi/internal/DefaultABTestContext.java
@@ -1,16 +1,26 @@
 package com.dianping.cat.abtest.spi.internal;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 
+import com.dianping.cat.Cat;
 import com.dianping.cat.abtest.spi.ABTestContext;
 import com.dianping.cat.abtest.spi.ABTestEntity;
+import com.dianping.cat.abtest.spi.ABTestGroupStrategy;
+import com.dianping.cat.message.Message;
+import com.dianping.cat.message.Transaction;
 
 public class DefaultABTestContext implements ABTestContext {
 	private String m_groupName = DEFAULT_GROUP;
 
+	private ABTestEntity m_entity;
+
 	private HttpServletRequest m_req;
 
-	private ABTestEntity m_entity;
+	private ABTestGroupStrategy m_groupStrategy;
+
+	private boolean m_applied;
 
 	public DefaultABTestContext(ABTestEntity entity) {
 		m_entity = entity;
@@ -18,6 +28,24 @@ public class DefaultABTestContext implements ABTestContext {
 
 	@Override
 	public String getGroupName() {
+		if (m_entity.isEligible(new Date())) {
+			if (!m_applied) {
+				Transaction t = Cat.newTransaction("GroupStrategy", m_entity.getGroupStrategy());
+
+				try {
+					m_groupStrategy.apply(this);
+
+					t.setStatus(Message.SUCCESS);
+				} catch (Throwable e) {
+					t.setStatus(e);
+					Cat.logError(e);
+				} finally {
+					t.complete();
+					m_applied = true;
+				}
+			}
+		}
+
 		return m_groupName;
 	}
 
@@ -38,5 +66,9 @@ public class DefaultABTestContext implements ABTestContext {
 	@Override
 	public ABTestEntity getEntity() {
 		return m_entity;
+	}
+
+	public void setGroupStrategy(ABTestGroupStrategy groupStrategy) {
+		m_groupStrategy = groupStrategy;
 	}
 }
