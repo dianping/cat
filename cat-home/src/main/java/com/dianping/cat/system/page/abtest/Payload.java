d25b94434187c9863a820329d73d4024063426f9
damon.zhu <hao.zhu@dianping.com>
2013-05-07 15:57:08 +0800

    change abtest database schema

--- cat-home/src/main/java/com/dianping/cat/system/page/abtest/Payload.java
+++ cat-home/src/main/java/com/dianping/cat/system/page/abtest/Payload.java
@@ -11,19 +11,25 @@ public class Payload implements ActionPayload<SystemPage, Action> {
 
 	@FieldMeta("op")
 	private Action m_action;
-	
+
 	@FieldMeta("status")
 	private String m_status;
-	
+
 	@FieldMeta("pageNum")
 	private int m_pageNum;
-	
+
+	@FieldMeta("enable")
+	private boolean m_enableAbtest;
+
+	@FieldMeta("ids")
+	private String m_ids;
+
 	public void setAction(String action) {
-		if(action.equalsIgnoreCase(Action.REPORT.getName())){
+		if (action.equalsIgnoreCase(Action.REPORT.getName())) {
 			m_action = Action.getByName(action, Action.REPORT);
-		}else if(action.equalsIgnoreCase(Action.LIST.getName())){
+		} else if (action.equalsIgnoreCase(Action.LIST.getName())) {
 			m_action = Action.getByName(action, Action.LIST);
-		}else{
+		} else {
 			m_action = Action.getByName(action, Action.VIEW);
 		}
 	}
@@ -32,28 +38,28 @@ public class Payload implements ActionPayload<SystemPage, Action> {
 	public Action getAction() {
 		return m_action;
 	}
-	
+
 	public String getStatus() {
-   	return m_status;
-   }
+		return m_status;
+	}
 
 	public void setStatus(String status) {
 		AbtestStatus abstatus = AbtestStatus.getByName(status, null);
 
-		if(abstatus != null){
+		if (abstatus != null) {
 			m_status = abstatus.name().toLowerCase();
-		}else{
+		} else {
 			m_status = "all";
 		}
-   }
+	}
 
 	public int getPageNum() {
-   	return m_pageNum;
-   }
+		return m_pageNum;
+	}
 
 	public void setPageNum(int pageNum) {
-   	m_pageNum = pageNum;
-   }
+		m_pageNum = pageNum;
+	}
 
 	@Override
 	public SystemPage getPage() {
@@ -69,10 +75,29 @@ public class Payload implements ActionPayload<SystemPage, Action> {
 		m_page = SystemPage.getByName(page, SystemPage.ABTEST);
 	}
 
+	public boolean isEnableAbtest() {
+		return m_enableAbtest;
+	}
+
+	public void setEnableAbtest(boolean enableAbtest) {
+		m_enableAbtest = enableAbtest;
+	}
+
+	public String getIds() {
+		return m_ids;
+	}
+
+	public void setIds(String ids) {
+		m_ids = ids;
+	}
+
 	@Override
 	public void validate(ActionContext<?> ctx) {
 		if (m_action == null) {
 			m_action = Action.VIEW;
+		}
+		if (m_status == null) {
+			m_status = "all";
 		}
 	}
 }
