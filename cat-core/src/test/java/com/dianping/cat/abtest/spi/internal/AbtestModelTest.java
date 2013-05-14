60f337effe521d1496f8510571d2fc22df7b78d8
ainilife <ainilife@gmail.com>
2013-05-14 20:31:43 +0800

    refactor cat-core according to the change of abtest model

--- cat-core/src/test/java/com/dianping/cat/abtest/spi/internal/AbtestModelTest.java
+++ cat-core/src/test/java/com/dianping/cat/abtest/spi/internal/AbtestModelTest.java
@@ -6,14 +6,14 @@ import java.io.InputStream;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
-import com.dianping.cat.abtest.model.entity.Abtest;
+import com.dianping.cat.abtest.model.entity.AbtestModel;
 import com.dianping.cat.abtest.model.transform.DefaultSaxParser;
 
 public class AbtestModelTest {
 	@Test
 	public void test() throws SAXException, IOException {
 		InputStream in = getClass().getResourceAsStream("abtest.xml");
-		Abtest abtest = DefaultSaxParser.parse(in);
+		AbtestModel abtest = DefaultSaxParser.parse(in);
 
 		System.out.println(abtest);
 	}
