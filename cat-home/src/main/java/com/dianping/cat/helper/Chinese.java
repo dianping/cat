7039ff24bbd0abd00393ba81d23968137bce7e32
yong.you <yong.you@dianping.com>
2013-08-28 16:48:10 +0800

    remove unused constants

--- cat-home/src/main/java/com/dianping/cat/helper/Chinese.java
+++ cat-home/src/main/java/com/dianping/cat/helper/Chinese.java
@@ -6,14 +6,6 @@ public class Chinese {
 
 	public static final String OFFLINE = "cat.qa.dianpingoa.com";
 
-	public static final String EXCEPTION_ALERT_TITLE = " CAT异常过多告警";
-
-	public static final String EXCEPTION_MANY = " 异常过多";
-
-	public static final String SERVICE_MANY = " 服务调用失败过多";
-
-	public static final String SERVICE_ALERT_TITLE = " CAT服务调用告警";
-
 	public static final String RESPONSE_TIME = "响应时间";
 
 	public static final String EXCEPTION_COUNT = "错误数";
