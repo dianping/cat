9fd0fc56278146bb929b46e07ddcc0e0fe925e48
yong.you <yong.you@dianping.com>
2013-09-16 17:46:12 +0800

    fix compile bug

--- cat-home/src/main/java/com/dianping/cat/system/page/abtest/Handler.java
+++ cat-home/src/main/java/com/dianping/cat/system/page/abtest/Handler.java
@@ -34,7 +34,6 @@ import com.dianping.cat.abtest.model.entity.Condition;
 import com.dianping.cat.abtest.model.entity.GroupstrategyDescriptor;
 import com.dianping.cat.abtest.model.entity.Run;
 import com.dianping.cat.core.dal.Project;
-import com.dianping.cat.home.abtest.ScriptFragementTest;
 import com.dianping.cat.home.dal.abtest.Abtest;
 import com.dianping.cat.home.dal.abtest.AbtestDao;
 import com.dianping.cat.home.dal.abtest.AbtestEntity;
@@ -352,7 +351,7 @@ public class Handler implements PageHandler<Context>, LogEnabled, Initializable 
 		m_configuration = new Configuration();
 		m_configuration.setDefaultEncoding("UTF-8");
 		try {
-			m_configuration.setClassForTemplateLoading(ScriptFragementTest.class, "/freemaker");
+			m_configuration.setClassForTemplateLoading(Handler.class, "/freemaker");
 		} catch (Exception e) {
 			Cat.logError(e);
 		}
