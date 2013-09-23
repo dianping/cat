449c382b7f024e30ae791b1b0e23e00baa570bc6
ainilife <ainilife@gmail.com>
2013-09-23 11:46:12 +0800

    Add a metricType into CatConstants

--- cat-client/src/main/java/com/dianping/cat/CatConstants.java
+++ cat-client/src/main/java/com/dianping/cat/CatConstants.java
@@ -69,6 +69,8 @@ public class CatConstants {
 	public static final String TYPE_URL_FORWARD = "URL.Forward";
 
 	public static final String TYPE_ACTION = "Action";
+	
+	public static final String TYPE_METRIC = "metricType";
 
 	public static final int ERROR_COUNT = 100;
 
