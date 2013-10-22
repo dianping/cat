1859bb5457c24ee0e716fe400cb4878ed418d57e
ainilife <ainilife@gmail.com>
2013-10-22 17:54:05 +0800

    add unit tests for transaction analyzer

--- cat-consumer/src/test/java/com/dianping/cat/consumer/transaction/TransactionAnalyzerTest.java
+++ cat-consumer/src/test/java/com/dianping/cat/consumer/transaction/TransactionAnalyzerTest.java
@@ -2,59 +2,53 @@ package com.dianping.cat.consumer.transaction;
 
 import junit.framework.Assert;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 import org.unidal.helper.Files;
 import org.unidal.lookup.ComponentTestCase;
 
-import com.dianping.cat.analysis.MessageAnalyzer;
+import com.dianping.cat.ServerConfigManager;
+import com.dianping.cat.consumer.MockReportManager;
 import com.dianping.cat.consumer.transaction.model.entity.TransactionReport;
 import com.dianping.cat.message.Message;
 import com.dianping.cat.message.internal.DefaultTransaction;
 import com.dianping.cat.message.spi.MessageTree;
 import com.dianping.cat.message.spi.internal.DefaultMessageTree;
+import com.dianping.cat.service.ReportDelegate;
+import com.dianping.cat.service.ReportManager;
 
-@Ignore
 public class TransactionAnalyzerTest extends ComponentTestCase {
-	private long timestamp;
+	private long m_timestamp;
+
+	private TransactionAnalyzer m_analyzer;
+
+	private String m_domain = "group";
+
+	@Before
+	public void init() {
+		m_timestamp = System.currentTimeMillis() - System.currentTimeMillis() % (3600 * 1000);
+
+		ReportDelegate<TransactionReport> transactionDelegate = new TransactionDelegate();
+		ServerConfigManager serverConfigManager = new ServerConfigManager();
+		ReportManager<TransactionReport> reportManager = new MockReportManager<TransactionReport>(transactionDelegate,
+		      m_domain);
+
+		m_analyzer = new TransactionAnalyzer();
+		m_analyzer.setDelegate((TransactionDelegate) transactionDelegate);
+		m_analyzer.setReportManager(reportManager);
+		m_analyzer.setServerConfigManager(serverConfigManager);
+	}
 
 	@Test
 	public void testProcessTransaction() throws Exception {
-		timestamp = System.currentTimeMillis() - System.currentTimeMillis() % (3600 * 1000);
-
-		TransactionAnalyzer analyzer = (TransactionAnalyzer) lookup(MessageAnalyzer.class, TransactionAnalyzer.ID);
-		TransactionReport report = new TransactionReport("Test");
 
 		for (int i = 1; i <= 1000; i++) {
 			MessageTree tree = newMessageTree(i);
-			DefaultTransaction t = new DefaultTransaction("A", "n" + i % 2, null);
-			DefaultTransaction t2 = new DefaultTransaction("A-1", "n" + i % 3, null);
 
-			if (i % 2 == 0) {
-				t2.setStatus("ERROR");
-			} else {
-				t2.setStatus(Message.SUCCESS);
-			}
-
-			t2.complete();
-			t2.setDurationInMillis(i);
-
-			t.addChild(t2);
-
-			if (i % 2 == 0) {
-				t.setStatus("ERROR");
-			} else {
-				t.setStatus(Message.SUCCESS);
-			}
-
-			t.complete();
-			t.setDurationInMillis(i * 2);
-			t.setTimestamp(timestamp + 1000);
-			t2.setTimestamp(timestamp + 2000);
-			tree.setMessage(t);
-
-			analyzer.processTransaction(report, tree, t);
+			m_analyzer.process(tree);
 		}
+
+		TransactionReport report = m_analyzer.getReport(m_domain);
 
 		report.accept(new TransactionStatisticsComputer());
 
@@ -66,9 +60,36 @@ public class TransactionAnalyzerTest extends ComponentTestCase {
 		MessageTree tree = new DefaultMessageTree();
 
 		tree.setMessageId("" + i);
-		tree.setDomain("group");
+		tree.setDomain(m_domain);
 		tree.setHostName("group001");
 		tree.setIpAddress("192.168.1.1");
+
+		DefaultTransaction t = new DefaultTransaction("A", "n" + i % 2, null);
+		DefaultTransaction t2 = new DefaultTransaction("A-1", "n" + i % 3, null);
+
+		if (i % 2 == 0) {
+			t2.setStatus("ERROR");
+		} else {
+			t2.setStatus(Message.SUCCESS);
+		}
+
+		t2.complete();
+		t2.setDurationInMillis(i);
+
+		t.addChild(t2);
+
+		if (i % 2 == 0) {
+			t.setStatus("ERROR");
+		} else {
+			t.setStatus(Message.SUCCESS);
+		}
+
+		t.complete();
+		t.setDurationInMillis(i * 2);
+		t.setTimestamp(m_timestamp + 1000);
+		t2.setTimestamp(m_timestamp + 2000);
+		tree.setMessage(t);
+
 		return tree;
 	}
 }
--- cat-consumer/src/test/java/com/dianping/cat/consumer/transaction/TransactionReportTest.java
+++ cat-consumer/src/test/java/com/dianping/cat/consumer/transaction/TransactionReportTest.java
@@ -1,7 +1,6 @@
 package com.dianping.cat.consumer.transaction;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.unidal.helper.Files;
 
@@ -9,7 +8,6 @@ import com.dianping.cat.consumer.transaction.model.entity.TransactionReport;
 import com.dianping.cat.consumer.transaction.model.transform.DefaultSaxParser;
 import com.dianping.cat.consumer.transaction.model.transform.DefaultXmlBuilder;
 
-@Ignore
 public class TransactionReportTest {
 	@Test
 	public void testXml() throws Exception {
