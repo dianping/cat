2e3133b94bb326a381ec5c73cf9fccb2afefc743
ainilife <ainilife@gmail.com>
2013-10-23 12:04:56 +0800

    add unit test for event analyzer

---
+++ cat-consumer/src/test/java/com/dianping/cat/consumer/event/EventAnalyzerTest.java
@@ -0,0 +1,98 @@
+package com.dianping.cat.consumer.event;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.unidal.helper.Files;
+import org.unidal.lookup.ComponentTestCase;
+
+import com.dianping.cat.consumer.MockReportManager;
+import com.dianping.cat.consumer.event.model.entity.EventReport;
+import com.dianping.cat.message.Message;
+import com.dianping.cat.message.internal.DefaultEvent;
+import com.dianping.cat.message.internal.DefaultTransaction;
+import com.dianping.cat.message.spi.MessageTree;
+import com.dianping.cat.message.spi.internal.DefaultMessageTree;
+import com.dianping.cat.service.ReportDelegate;
+import com.dianping.cat.service.ReportManager;
+
+public class EventAnalyzerTest extends ComponentTestCase {
+
+	private long m_timestamp;
+
+	private EventAnalyzer m_analyzer;
+
+	private String m_domain = "group";
+
+	@Before
+	public void init() {
+		m_timestamp = System.currentTimeMillis() - System.currentTimeMillis() % (3600 * 1000);
+
+		ReportDelegate<EventReport> deleaate = new EventDelegate();
+		ReportManager<EventReport> reportManager = new MockReportManager<EventReport>(deleaate, m_domain);
+
+		m_analyzer = new EventAnalyzer();
+		m_analyzer.setReportManager(reportManager);
+	}
+
+	@Test
+	public void testProcessTransaction() throws Exception {
+
+		for (int i = 1; i <= 1000; i++) {
+			MessageTree tree = newMessageTree(i);
+
+			m_analyzer.process(tree);
+		}
+
+		EventReport report = m_analyzer.getReport(m_domain);
+		
+		String expected = Files.forIO().readFrom(getClass().getResourceAsStream("EventAnalyzerTest.xml"), "utf-8");
+		Assert.assertEquals(expected.replaceAll("\\s*", ""), report.toString().replaceAll("\\s*", ""));
+	}
+
+	protected MessageTree newMessageTree(int i) {
+		MessageTree tree = new DefaultMessageTree();
+
+		tree.setMessageId("" + i);
+		tree.setDomain(m_domain);
+		tree.setHostName("group001");
+		tree.setIpAddress("192.168.1.1");
+
+		DefaultTransaction t = new DefaultTransaction("A", "n" + i % 2, null);
+		DefaultTransaction t2 = new DefaultTransaction("A-1", "n" + i % 3, null);
+
+		if (i % 2 == 0) {
+			t2.setStatus("ERROR");
+		} else {
+			t2.setStatus(Message.SUCCESS);
+		}
+
+		Message event1 = new DefaultEvent("test2", "fail");
+
+		t2.addChild(event1);
+		t2.complete();
+		t2.setDurationInMillis(i);
+
+		t.addChild(t2);
+
+		if (i % 2 == 0) {
+			t.setStatus("ERROR");
+		} else {
+			t.setStatus(Message.SUCCESS);
+		}
+
+		Message event = new DefaultEvent("test1", "success");
+		event.setStatus(Message.SUCCESS);
+		t.addChild(event);
+
+		t.complete();
+		t.setDurationInMillis(i * 2);
+		t.setTimestamp(m_timestamp + 1000);
+		t2.setTimestamp(m_timestamp + 2000);
+		tree.setMessage(t);
+
+		return tree;
+	}
+
+}
