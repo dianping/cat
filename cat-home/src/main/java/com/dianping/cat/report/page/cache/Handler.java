bee0e897fe746a06b201b6736500c955f9d794f4
yong.you <yong.you@dianping.com>
2013-10-24 15:47:24 +0800

    modify the cat cache report

--- cat-home/src/main/java/com/dianping/cat/report/page/cache/Handler.java
+++ cat-home/src/main/java/com/dianping/cat/report/page/cache/Handler.java
@@ -20,25 +20,20 @@ import org.unidal.web.mvc.annotation.PayloadMeta;
 import com.dianping.cat.Cat;
 import com.dianping.cat.Constants;
 import com.dianping.cat.consumer.event.EventAnalyzer;
-import com.dianping.cat.consumer.event.EventReportMerger;
 import com.dianping.cat.consumer.event.model.entity.EventReport;
 import com.dianping.cat.consumer.transaction.TransactionAnalyzer;
-import com.dianping.cat.consumer.transaction.TransactionReportMerger;
 import com.dianping.cat.consumer.transaction.model.entity.TransactionReport;
 import com.dianping.cat.consumer.transaction.model.entity.TransactionType;
 import com.dianping.cat.consumer.transaction.model.transform.BaseVisitor;
-import com.dianping.cat.helper.TimeUtil;
 import com.dianping.cat.report.ReportPage;
 import com.dianping.cat.report.page.PayloadNormalizer;
 import com.dianping.cat.report.page.PieChart;
 import com.dianping.cat.report.page.PieChart.Item;
 import com.dianping.cat.report.page.cache.CacheReport.CacheNameItem;
-import com.dianping.cat.report.page.event.EventMergeManager;
 import com.dianping.cat.report.page.event.TpsStatistics;
 import com.dianping.cat.report.page.model.spi.ModelService;
 import com.dianping.cat.report.page.transaction.MergeAllMachine;
 import com.dianping.cat.report.page.transaction.MergeAllName;
-import com.dianping.cat.report.page.transaction.TransactionMergeManager;
 import com.dianping.cat.report.service.ReportService;
 import com.dianping.cat.service.ModelRequest;
 import com.dianping.cat.service.ModelResponse;
@@ -55,21 +50,20 @@ public class Handler implements PageHandler<Context> {
 	private ReportService m_reportService;
 
 	@Inject
-	private TransactionMergeManager m_transactionMergeManger;
-
-	@Inject
-	private EventMergeManager m_eventMergerMergeManager;
-
-	@Inject
 	private PayloadNormalizer m_normalizePayload;
 
 	@Inject(type = ModelService.class, value = TransactionAnalyzer.ID)
 	private ModelService<TransactionReport> m_transactionService;
 
-	private Set<String> m_cacheTypes;
+	private CacheReport buildCacheReport(TransactionReport transactionReport, EventReport eventReport, Payload payload) {
+		String type = payload.getType();
+		String queryName = payload.getQueryName();
+		String ip  =payload.getIpAddress();
+		String sortBy = payload.getSortBy();
+		ReportVisitor visitor = new ReportVisitor();
+		
+		visitor.visitTransactionReport(transactionReport);
 
-	private CacheReport buildCacheReport(TransactionReport transactionReport, EventReport eventReport, String type,
-	      String sortBy, String queryName, String ip) {
 		TransactionReportVistor vistor = new TransactionReportVistor();
 
 		vistor.setType(type).setQueryName(queryName).setSortBy(sortBy).setCurrentIp(ip);
@@ -135,71 +129,65 @@ public class Handler implements PageHandler<Context> {
 		String domain = payload.getDomain();
 		String ipAddress = payload.getIpAddress();
 		String type = payload.getType();
-
 		ModelRequest request = new ModelRequest(domain, payload.getDate()) //
 		      .setProperty("ip", ipAddress);
+		EventReport eventReport = null;
 
 		if (StringUtils.isEmpty(type)) {
-			EventReportMerger merger = new EventReportMerger(new EventReport(domain));
+			ModelResponse<EventReport> response = m_eventService.invoke(request);
 
-			for (String cacheType : m_cacheTypes) {
-				request.setProperty("type", cacheType);
-				ModelResponse<EventReport> response = m_eventService.invoke(request);
-				EventReport eventReport = response.getModel();
-
-				merger.visitEventReport(eventReport);
-			}
-			EventReport eventReport = merger.getEventReport();
-
-			eventReport = m_eventMergerMergeManager.mergerAllIp(eventReport, ipAddress);
-			return eventReport;
-
+			eventReport = response.getModel();
 		} else {
 			request.setProperty("type", type);
 			ModelResponse<EventReport> response = m_eventService.invoke(request);
-			EventReport eventReport = response.getModel();
 
-			eventReport = m_eventMergerMergeManager.mergerAllIp(eventReport, ipAddress);
-			return eventReport;
+			eventReport = response.getModel();
 		}
+		if (Constants.ALL.equalsIgnoreCase(ipAddress)) {
+			com.dianping.cat.report.page.event.MergeAllMachine allEvent = new com.dianping.cat.report.page.event.MergeAllMachine();
+
+			allEvent.visitEventReport(eventReport);
+			eventReport = allEvent.getReport();
+		}
+		if (Constants.ALL.equalsIgnoreCase(type)) {
+			com.dianping.cat.report.page.event.MergeAllName allEvent = new com.dianping.cat.report.page.event.MergeAllName();
+
+			allEvent.visitEventReport(eventReport);
+			eventReport = allEvent.getReport();
+		}
+
+		return eventReport;
 	}
 
 	private TransactionReport getHourlyTransactionReport(Payload payload) {
 		String domain = payload.getDomain();
 		String ipAddress = payload.getIpAddress();
 		String type = payload.getType();
-
 		ModelRequest request = new ModelRequest(domain, payload.getDate()) //
 		      .setProperty("ip", ipAddress);
+		TransactionReport transactionReport = null;
 
-		ModelResponse<TransactionReport> all = m_transactionService.invoke(request);
-		TransactionReport report = all.getModel();
-		ReportVisitor visitor = new ReportVisitor();
-		visitor.visitTransactionReport(report);
-
-		m_cacheTypes = visitor.getCacheTypes();
 		if (StringUtils.isEmpty(type)) {
-			TransactionReportMerger merger = new TransactionReportMerger(new TransactionReport(domain));
-
-			for (String temp : m_cacheTypes) {
-				request.setProperty("type", temp);
-				ModelResponse<TransactionReport> response = m_transactionService.invoke(request);
-				TransactionReport transactionReport = response.getModel();
-
-				merger.visitTransactionReport(transactionReport);
-			}
-
-			TransactionReport transactionReport = merger.getTransactionReport();
-			transactionReport = m_transactionMergeManger.mergerAllIp(transactionReport, ipAddress);
-			return transactionReport;
+			ModelResponse<TransactionReport> response = m_transactionService.invoke(request);
+			transactionReport = response.getModel();
 		} else {
 			request.setProperty("type", type);
 			ModelResponse<TransactionReport> response = m_transactionService.invoke(request);
-			TransactionReport transactionReport = response.getModel();
-
-			transactionReport = m_transactionMergeManger.mergerAllIp(transactionReport, ipAddress);
-			return transactionReport;
+			transactionReport = response.getModel();
 		}
+		if (Constants.ALL.equalsIgnoreCase(ipAddress)) {
+			MergeAllMachine all = new MergeAllMachine();
+
+			all.visitTransactionReport(transactionReport);
+			transactionReport = all.getReport();
+		}
+		if (Constants.ALL.equalsIgnoreCase(type)) {
+			MergeAllName all = new MergeAllName();
+
+			all.visitTransactionReport(transactionReport);
+			transactionReport = all.getReport();
+		}
+		return transactionReport;
 	}
 
 	@Override
@@ -214,65 +202,25 @@ public class Handler implements PageHandler<Context> {
 	public void handleOutbound(Context ctx) throws ServletException, IOException {
 		Model model = new Model(ctx);
 		Payload payload = ctx.getPayload();
-		String ipAddress = payload.getIpAddress();
 		String type = payload.getType();
+		TransactionReport transactionReport = null;
+		EventReport eventReport = null;
 
 		normalize(model, payload);
 		switch (payload.getAction()) {
 		case HOURLY_REPORT:
-			TransactionReport transactionReport = getHourlyTransactionReport(payload);
-			EventReport eventReport = getHourlyEventReport(payload);
-
-			if (payload.getPeriod().isLast()) {
-				Set<String> domains = m_reportService.queryAllDomainNames(new Date(payload.getDate()),
-				      new Date(payload.getDate() + TimeUtil.ONE_HOUR), TransactionAnalyzer.ID);
-				Set<String> domainNames = transactionReport.getDomainNames();
-
-				domainNames.addAll(domains);
-			}
-			if (Constants.ALL.equalsIgnoreCase(ipAddress)) {
-				MergeAllMachine all = new MergeAllMachine();
-
-				all.visitTransactionReport(transactionReport);
-				transactionReport = all.getReport();
-
-				com.dianping.cat.report.page.event.MergeAllMachine allEvent = new com.dianping.cat.report.page.event.MergeAllMachine();
-
-				allEvent.visitEventReport(eventReport);
-				eventReport = allEvent.getReport();
-			}
-
-			if (Constants.ALL.equalsIgnoreCase(type)) {
-				MergeAllName all = new MergeAllName();
-
-				all.visitTransactionReport(transactionReport);
-				transactionReport = all.getReport();
-
-				com.dianping.cat.report.page.event.MergeAllName allEvent = new com.dianping.cat.report.page.event.MergeAllName();
-
-				allEvent.visitEventReport(eventReport);
-				eventReport = allEvent.getReport();
-			}
-
-			calculateEventTps(payload, eventReport);
-			calculateTransactionTps(payload, transactionReport);
-			CacheReport cacheReport = buildCacheReport(transactionReport, eventReport, payload.getType(),
-			      payload.getSortBy(), payload.getQueryName(), payload.getIpAddress());
-
-			model.setReport(cacheReport);
+			transactionReport = getHourlyTransactionReport(payload);
+			eventReport = getHourlyEventReport(payload);
 			break;
 		case HISTORY_REPORT:
-			TransactionReport transactionHistoryReport = getHistoryTransactionReport(payload);
-			EventReport eventHistoryReport = getHistoryEventReport(payload);
-
-			calculateEventTps(payload, eventHistoryReport);
-			calculateTransactionTps(payload, transactionHistoryReport);
-			CacheReport cacheHistoryReport = buildCacheReport(transactionHistoryReport, eventHistoryReport,
-			      payload.getType(), payload.getSortBy(), payload.getQueryName(), payload.getIpAddress());
-
-			model.setReport(cacheHistoryReport);
+			transactionReport = getHistoryTransactionReport(payload);
+			eventReport = getHistoryEventReport(payload);
 			break;
 		}
+		calculateEventTps(payload, eventReport);
+		calculateTransactionTps(payload, transactionReport);
+		CacheReport cacheReport = buildCacheReport(transactionReport, eventReport, payload);
+		model.setReport(cacheReport);
 		if (!StringUtil.isEmpty(type)) {
 			model.setPieChart(buildPieChart(model.getReport()));
 		}
