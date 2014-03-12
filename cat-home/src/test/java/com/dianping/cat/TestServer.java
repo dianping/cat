2c19575bd085b69c6c98b2483e9a4abd8d7f941c
曾伟伟 <weiwei.zeng@dianping.com>
2014-03-12 11:03:37 +0800

    pull request test

--- cat-home/src/test/java/com/dianping/cat/TestServer.java
+++ cat-home/src/test/java/com/dianping/cat/TestServer.java
@@ -13,6 +13,9 @@ import org.unidal.test.jetty.JettyServer;
 public class TestServer extends JettyServer {
 	public static void main(String[] args) throws Exception {
 		TestServer server = new TestServer();
+
+        System.out.println("test");
+        
 		System.setProperty("devMode", "true");
 		server.startServer();
 		server.startWebApp();
