d47bccf8f892fd3e002f8575cf25f9d4fadfffa8
youyong205 <youyong205@126.com>
2014-03-20 13:40:03 +0800

    modify the cat email send error

--- cat-home/src/main/java/com/dianping/cat/system/tool/MailSMS.java
+++ cat-home/src/main/java/com/dianping/cat/system/tool/MailSMS.java
@@ -5,5 +5,7 @@ import java.util.List;
 public interface MailSMS {
 
 	public boolean sendEmail(String title, String content, List<String> emails);
+	
+	public boolean sendEmailByGmail(String title,String content,List<String> emails);
 
 }
