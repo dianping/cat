afb5f8d4bbc911678f4b8ca35dcfea1b2d3336c4
youyong205 <youyong205@126.com>
2014-03-31 10:51:18 +0800

    fix oom problem

--- cat-core/src/main/java/com/dianping/cat/analysis/PeriodTask.java
+++ cat-core/src/main/java/com/dianping/cat/analysis/PeriodTask.java
@@ -55,6 +55,7 @@ public class PeriodTask implements Task, LogEnabled {
 	public void finish() {
 		try {
 			m_analyzer.doCheckpoint(true);
+			m_analyzer.destroy();
 		} catch (Exception e) {
 			Cat.logError(e);
 		}
