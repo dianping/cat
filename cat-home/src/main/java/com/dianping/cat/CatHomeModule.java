70c3c9ccef1c1cbc943084966714007ea6f69507
leon.li <leon.li@dianping.com>
2014-06-05 16:45:39 +0800

    remove ProjectUpdateTask run

--- cat-home/src/main/java/com/dianping/cat/CatHomeModule.java
+++ cat-home/src/main/java/com/dianping/cat/CatHomeModule.java
@@ -59,8 +59,6 @@ public class CatHomeModule extends AbstractModule {
 			Threads.forGroup("Cat").start(exceptionAlert);
 			Threads.forGroup("Cat").start(productUpdateTask);
 		}
-		ProjectUpdateTask productUpdateTask = ctx.lookup(ProjectUpdateTask.class);
-		Threads.forGroup("Cat").start(productUpdateTask);
 		executeAlarmModule(ctx);
 	}
 
