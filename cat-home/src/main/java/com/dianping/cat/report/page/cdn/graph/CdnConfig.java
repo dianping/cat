e641b732312972908b7fec44c083dea33c34a9e7
youyong205 <youyong205@126.com>
2014-06-18 15:07:11 +0800

    modify the cdn ip

--- cat-home/src/main/java/com/dianping/cat/report/page/cdn/graph/CdnConfig.java
+++ cat-home/src/main/java/com/dianping/cat/report/page/cdn/graph/CdnConfig.java
@@ -7,19 +7,15 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class CdnConfig {
+	public final String GROUP = "cdn";
+	
 	private static final Map<String, String> CDNS = new HashMap<String, String>() {
 		private static final long serialVersionUID = 1L;
 		{
-			put("10.1.1.1", "帝联");
-			put("10.1.1.2", "网宿");
+			put("180.153.132.55", "帝联");
+			put("180.153.132.56", "网宿");
 		}
 	};
-	
-	public final String GROUP = "cdn";
-	
-	public String getCdnName(String vip) {
-		return CDNS.get(vip);
-	}
 	
 	public List<String> getAllCdnNames() {
 		List<String> cdns = new ArrayList<String>();
@@ -28,6 +24,10 @@ public class CdnConfig {
 			cdns.add(entry.getValue());
 		}
 		return cdns;
+	}
+	
+	public String getCdnName(String vip) {
+		return CDNS.get(vip);
 	}
 	
 	public boolean validateVip(String vip) {
