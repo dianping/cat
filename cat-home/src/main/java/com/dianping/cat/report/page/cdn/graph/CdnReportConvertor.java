2477d770ec8af1e1e896d9faf2ed0eb05fe1bd02
曾伟伟 <weiwei.zeng@dianping.com>
2014-06-24 11:19:06 +0800

    confict fix

--- cat-home/src/main/java/com/dianping/cat/report/page/cdn/graph/CdnReportConvertor.java
+++ cat-home/src/main/java/com/dianping/cat/report/page/cdn/graph/CdnReportConvertor.java
@@ -8,7 +8,6 @@ import com.dianping.cat.service.IpService;
 import com.dianping.cat.service.IpService.IpInfo;
 
 public class CdnReportConvertor extends BaseVisitor {
-	
 	private IpService m_ipService;
 
 	private MetricReport m_report;
