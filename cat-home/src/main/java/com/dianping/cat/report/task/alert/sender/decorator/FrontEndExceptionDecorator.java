7d2943bc518f3b07aa3d382fb70eedefcc03c95d
jialinsun <jialin.sun@dianping.com>
2014-07-30 16:32:20 +0800

    modify mail content

--- cat-home/src/main/java/com/dianping/cat/report/task/alert/sender/decorator/FrontEndExceptionDecorator.java
+++ cat-home/src/main/java/com/dianping/cat/report/task/alert/sender/decorator/FrontEndExceptionDecorator.java
@@ -1,6 +1,11 @@
 package com.dianping.cat.report.task.alert.sender.decorator;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.dianping.cat.Constants;
 import com.dianping.cat.report.task.alert.AlertConstants;
+import com.dianping.cat.report.task.alert.sender.AlertEntity;
 
 public class FrontEndExceptionDecorator extends ExceptionDecorator {
 
@@ -9,6 +14,27 @@ public class FrontEndExceptionDecorator extends ExceptionDecorator {
 	@Override
 	public String getId() {
 		return ID;
+	}
+
+	@Override
+	public String generateTitle(AlertEntity alert) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[CAT异常告警] [项目: ").append(Constants.FRONT_END).append("]");
+		return sb.toString();
+	}
+
+	@Override
+	protected Map<Object, Object> generateExceptionMap(AlertEntity alert) {
+		String domain = Constants.FRONT_END;
+		String contactInfo = buildContactInfo(domain);
+		Map<Object, Object> map = new HashMap<Object, Object>();
+
+		map.put("domain", domain);
+		map.put("content", alert.getContent());
+		map.put("date", m_dateFormat.format(alert.getDate()));
+		map.put("contactInfo", contactInfo);
+
+		return map;
 	}
 
 	@Override
