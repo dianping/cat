06cad87ab89f004f9bcdbb92ed488d6dde60e081
jialinsun <jialin.sun@dianping.com>
2014-08-04 11:11:41 +0800

    chengjunxia bootcamp task modify & merge request

--- cat-home/src/main/java/com/dianping/cat/report/page/metric/JspFile.java
+++ cat-home/src/main/java/com/dianping/cat/report/page/metric/JspFile.java
@@ -3,7 +3,9 @@ package com.dianping.cat.report.page.metric;
 public enum JspFile {
 	METRIC("/jsp/report/metric/metric.jsp"),
 
-	DASHBOARD("/jsp/report/metric/dashboard.jsp"), ;
+	DASHBOARD("/jsp/report/metric/dashboard.jsp"),
+
+	JSON("/jsp/report/metric/json.jsp");
 
 	private String m_path;
 
