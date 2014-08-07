52f8f66f53f6388b7dcd10c9705c7f3f215e329f
youyong205 <youyong205@126.com>
2014-08-07 11:01:45 +0800

    modify the test

--- cat-client/src/test/java/com/dianping/cat/AllTests.java
+++ cat-client/src/test/java/com/dianping/cat/AllTests.java
@@ -9,7 +9,6 @@ import com.dianping.cat.message.EventTest;
 import com.dianping.cat.message.HeartbeatTest;
 import com.dianping.cat.message.MessageTest;
 import com.dianping.cat.message.TransactionTest;
-import com.dianping.cat.message.internal.MessageIdFactoryTest;
 import com.dianping.cat.message.internal.MockMessageBuilderTest;
 import com.dianping.cat.message.spi.codec.PlainTextMessageCodecTest;
 import com.dianping.cat.servlet.CatFilterTest;
