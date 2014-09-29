d88cf54560f43df46e6cd8a6a305dcb8e3398d82
leon.li <leon.li@dianping.com>
2014-09-29 17:54:14 +0800

    remove logs about sending alert message

--- cat-home/src/main/java/com/dianping/cat/report/task/alert/sender/sender/SenderManager.java
+++ cat-home/src/main/java/com/dianping/cat/report/task/alert/sender/sender/SenderManager.java
@@ -3,8 +3,6 @@ package com.dianping.cat.report.task.alert.sender.sender;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.codehaus.plexus.logging.LogEnabled;
-import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 import org.unidal.lookup.ContainerHolder;
@@ -14,11 +12,9 @@ import com.dianping.cat.message.Event;
 import com.dianping.cat.report.task.alert.sender.AlertChannel;
 import com.dianping.cat.report.task.alert.sender.AlertMessageEntity;
 
-public class SenderManager extends ContainerHolder implements Initializable, LogEnabled {
+public class SenderManager extends ContainerHolder implements Initializable {
 
 	private Map<String, Sender> m_senders = new HashMap<String, Sender>();
-
-	private Logger m_logger;
 
 	@Override
 	public void initialize() throws InitializationException {
@@ -38,17 +34,11 @@ public class SenderManager extends ContainerHolder implements Initializable, Log
 			} else {
 				Cat.logEvent("Channel:" + channelName, type + ":fail", Event.SUCCESS, null);
 			}
-			m_logger.info("Alert Channel " + channelName + ",content" + message.toString());
 			return result;
 		} catch (Exception e) {
 			Cat.logError(e);
 			return false;
 		}
-	}
-
-	@Override
-	public void enableLogging(Logger logger) {
-		m_logger = logger;
 	}
 
 }
