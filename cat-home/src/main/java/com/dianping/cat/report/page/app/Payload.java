0f94b97e596518f06f0e7e8dbc8f79b405c93200
leon.li <leon.li@dianping.com>
2014-10-10 17:10:27 +0800

    add activity page for app

--- cat-home/src/main/java/com/dianping/cat/report/page/app/Payload.java
+++ cat-home/src/main/java/com/dianping/cat/report/page/app/Payload.java
@@ -1,13 +1,13 @@
 package com.dianping.cat.report.page.app;
 
+import org.unidal.web.mvc.ActionContext;
+import org.unidal.web.mvc.payload.annotation.FieldMeta;
+
+import com.dianping.cat.config.app.AppDataGroupByField;
 import com.dianping.cat.config.app.AppDataService;
 import com.dianping.cat.config.app.QueryEntity;
 import com.dianping.cat.report.ReportPage;
 import com.dianping.cat.report.page.AbstractReportPayload;
-import com.dianping.cat.config.app.AppDataGroupByField;
-
-import org.unidal.web.mvc.ActionContext;
-import org.unidal.web.mvc.payload.annotation.FieldMeta;
 
 public class Payload extends AbstractReportPayload<Action> {
 	private ReportPage m_page;
@@ -30,8 +30,33 @@ public class Payload extends AbstractReportPayload<Action> {
 	@FieldMeta("sort")
 	private String m_sort = AppDataService.SUCCESS;
 
+	@FieldMeta("showActivity")
+	private boolean m_showActivity;
+
 	public Payload() {
 		super(ReportPage.APP);
+	}
+
+	@Override
+	public Action getAction() {
+		return m_action;
+	}
+
+	public AppDataGroupByField getGroupByField() {
+		return m_groupByField;
+	}
+
+	@Override
+	public ReportPage getPage() {
+		return m_page;
+	}
+
+	public String getQuery1() {
+		return m_query1;
+	}
+
+	public String getQuery2() {
+		return m_query2;
 	}
 
 	public QueryEntity getQueryEntity1() {
@@ -50,18 +75,24 @@ public class Payload extends AbstractReportPayload<Action> {
 		}
 	}
 
-	@Override
-	public Action getAction() {
-		return m_action;
+	public String getSort() {
+		return m_sort;
 	}
 
-	@Override
-	public ReportPage getPage() {
-		return m_page;
+	public String getType() {
+		return m_type;
+	}
+
+	public boolean isShowActivity() {
+		return m_showActivity;
 	}
 
 	public void setAction(String action) {
 		m_action = Action.getByName(action, Action.VIEW);
+	}
+
+	public void setGroupByField(String groupByField) {
+		m_groupByField = AppDataGroupByField.getByName(groupByField, AppDataGroupByField.CODE);
 	}
 
 	@Override
@@ -69,44 +100,24 @@ public class Payload extends AbstractReportPayload<Action> {
 		m_page = ReportPage.getByName(page, ReportPage.APP);
 	}
 
-	public void setGroupByField(String groupByField) {
-		m_groupByField = AppDataGroupByField.getByName(groupByField, AppDataGroupByField.CODE);
-	}
-
-	public AppDataGroupByField getGroupByField() {
-		return m_groupByField;
-	}
-
-	public String getQuery1() {
-		return m_query1;
-	}
-
 	public void setQuery1(String query1) {
 		m_query1 = query1;
-	}
-
-	public String getQuery2() {
-		return m_query2;
 	}
 
 	public void setQuery2(String query2) {
 		m_query2 = query2;
 	}
 
-	public void setType(String type) {
-		m_type = type;
-	}
-
-	public String getType() {
-		return m_type;
-	}
-
-	public String getSort() {
-		return m_sort;
+	public void setShowActivity(boolean showActivity) {
+		m_showActivity = showActivity;
 	}
 
 	public void setSort(String sort) {
 		m_sort = sort;
+	}
+
+	public void setType(String type) {
+		m_type = type;
 	}
 
 	@Override
