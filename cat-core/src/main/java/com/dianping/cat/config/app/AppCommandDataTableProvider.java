914d919ce225e7ab67d3d3d5dd23f745e13ee825
youyong205 <youyong205@126.com>
2014-11-21 14:28:28 +0800

    modify the provider

--- cat-core/src/main/java/com/dianping/cat/config/app/AppCommandDataTableProvider.java
+++ cat-core/src/main/java/com/dianping/cat/config/app/AppCommandDataTableProvider.java
@@ -37,7 +37,7 @@ public class AppCommandDataTableProvider implements TableProvider {
 		Date old = new Date();
 
 		try {
-			old = new SimpleDateFormat("yyyy-MM-dd").parse("2014-11-20");
+			old = new SimpleDateFormat("yyyy-MM-dd").parse("2015-01-01");
 		} catch (ParseException e) {
 			throw new RuntimeException(e);
 		}
