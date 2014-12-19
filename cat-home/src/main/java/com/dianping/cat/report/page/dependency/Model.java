a5845b7332e3c6e6d1cc2a2d7e144aa121592295
youyong205 <youyong205@126.com>
2014-12-19 22:06:02 +0800

    modify the status

--- cat-home/src/main/java/com/dianping/cat/report/page/dependency/Model.java
+++ cat-home/src/main/java/com/dianping/cat/report/page/dependency/Model.java
@@ -18,7 +18,7 @@ import com.dianping.cat.consumer.top.model.entity.TopReport;
 import com.dianping.cat.helper.SortHelper;
 import com.dianping.cat.report.page.AbstractReportModel;
 import com.dianping.cat.report.page.LineChart;
-import com.dianping.cat.report.page.dependency.dashboard.ProductLinesDashboard;
+import com.dianping.cat.report.page.dependency.graph.ProductLinesDashboard;
 
 @ModelMeta(DependencyAnalyzer.ID)
 public class Model extends AbstractReportModel<Action, Context> {
