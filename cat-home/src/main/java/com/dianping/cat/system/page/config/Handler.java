3a1344a9c4819897c7342b5fc159dd9a6d3732d4
youyong205 <youyong205@126.com>
2015-01-02 09:29:25 +0800

    modify the handler

--- cat-home/src/main/java/com/dianping/cat/system/page/config/Handler.java
+++ cat-home/src/main/java/com/dianping/cat/system/page/config/Handler.java
@@ -12,7 +12,6 @@ import org.unidal.web.mvc.PageHandler;
 import org.unidal.web.mvc.annotation.InboundActionMeta;
 import org.unidal.web.mvc.annotation.OutboundActionMeta;
 import org.unidal.web.mvc.annotation.PayloadMeta;
-import org.unidal.web.mvc.annotation.PreInboundActionMeta;
 
 import com.dianping.cat.Cat;
 import com.dianping.cat.home.dal.report.ConfigModification;
