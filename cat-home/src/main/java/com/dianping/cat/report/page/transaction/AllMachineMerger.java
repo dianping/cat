988085f3f6d0db56f2e78ca05b08bf9987388d47
youyong205 <youyong205@126.com>
2015-02-05 22:28:18 +0800

    rename merge

--- cat-home/src/main/java/com/dianping/cat/report/page/transaction/AllMachineMerger.java
+++ cat-home/src/main/java/com/dianping/cat/report/page/transaction/AllMachineMerger.java
@@ -10,7 +10,7 @@ import com.dianping.cat.consumer.transaction.model.entity.TransactionReport;
 import com.dianping.cat.consumer.transaction.model.entity.TransactionType;
 import com.dianping.cat.consumer.transaction.model.transform.BaseVisitor;
 
-public class MergeAllMachine extends BaseVisitor {
+public class AllMachineMerger extends BaseVisitor {
 
 	public TransactionReport m_report;
 
