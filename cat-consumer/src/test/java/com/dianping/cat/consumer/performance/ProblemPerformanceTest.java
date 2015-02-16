bceed9043b8cfb7a699f630f6a2d488590092638
youyong205 <youyong205@126.com>
2015-02-16 18:13:16 +0800

    format code

--- cat-consumer/src/test/java/com/dianping/cat/consumer/performance/ProblemPerformanceTest.java
+++ cat-consumer/src/test/java/com/dianping/cat/consumer/performance/ProblemPerformanceTest.java
@@ -21,7 +21,7 @@ public class ProblemPerformanceTest extends ComponentTestCase {
 
 		long current = System.currentTimeMillis();
 
-		long size = 1000000000l;
+		long size = 10000000l;
 		for (int i = 0; i < size; i++) {
 			analyzer.process(tree);
 		}
@@ -61,46 +61,44 @@ public class ProblemPerformanceTest extends ComponentTestCase {
 			@Override
 			public MessageHolder define() {
 				TransactionHolder t = t("WEB CLUSTER", "GET", 112819)
-				      //
 				      .at(1348374838231L)
-				      //
-				      .after(1300).child(t("QUICKIE SERVICE", "gimme_stuff", 1571))
-				      //
-				      .after(100).child(e("SERVICE", "event1")).child(e("Excetion", "NullPointException1")).after(100)
+				      .after(1300)
+				      .child(t("QUICKIE SERVICE", "gimme_stuff", 1571))
+				      .after(100)
+				      .child(e("SERVICE", "event1"))
+				      .child(e("Excetion", "NullPointException1"))
+				      .after(100)
 				      .child(h("SERVICE", "heartbeat1"))
-				      //
-				      .after(100).child(t("WEB SERVER", "GET", 109358)
-				      //
-				            .after(1000).child(t("SOME SERVICE", "get", 4345) //
-				                  .after(4000).child(t("MEMCACHED", "Get", 279)))
-				            //
-				            .mark().after(200).child(t("MEMCACHED", "Inc", 319))
-				            //
-				            .reset().after(500).child(t("BIG ASS SERVICE", "getThemDatar", 97155)
-				            //
-				                  .after(1000).mark().child(t("SERVICE", "getStuff", 3760))
-				                  //
-				                  .reset().child(t("DATAR", "findThings", 94537))
-				                  //
-				                  .after(200).child(t("THINGIE", "getMoar", 1435)) //
-				            )
-				            //
-				            .after(100).mark().child(t("OTHER DATA SERVICE", "get", 4394)
-				            //
-				                  .after(1000).mark().child(t("MEMCACHED", "Get", 378))
-				                  //
-				                  .reset().child(t("MEMCACHED", "Get", 3496)) //
-				            )
-				            //
-				            .reset().child(t("FINAL DATA SERVICE", "get", 4394)
-				            //
-				                  .after(1000).mark().child(t("MEMCACHED", "Get", 386))
-				                  //
-				                  .reset().child(t("MEMCACHED", "Get", 322))
-				                  //
-				                  .reset().child(t("MEMCACHED", "Get", 322)).child(e("Excetion", "NullPointException"))) //
-				      ) //
-				;
+				      .after(100)
+				      .child(
+				            t("WEB SERVER", "GET", 109358)
+				                  .after(1000)
+				                  .child(t("SOME SERVICE", "get", 4345) //
+				                        .after(4000).child(t("MEMCACHED", "Get", 279)))
+				                  .mark()
+				                  .after(200)
+				                  .child(t("MEMCACHED", "Inc", 319))
+				                  .reset()
+				                  .after(500)
+				                  .child(
+				                        t("BIG ASS SERVICE", "getThemDatar", 97155).after(1000).mark()
+				                              .child(t("SERVICE", "getStuff", 3760)).reset()
+				                              .child(t("DATAR", "findThings", 94537)).after(200)
+				                              .child(t("THINGIE", "getMoar", 1435)) //
+				                  )
+				                  .after(100)
+				                  .mark()
+				                  .child(
+				                        t("OTHER DATA SERVICE", "get", 4394).after(1000).mark()
+				                              .child(t("MEMCACHED", "Get", 378)).reset().child(t("MEMCACHED", "Get", 3496)) //
+				                  )
+				                  .reset()
+				                  .child(
+				                        t("FINAL DATA SERVICE", "get", 4394).after(1000).mark()
+				                              .child(t("MEMCACHED", "Get", 386)).reset().child(t("MEMCACHED", "Get", 322))
+				                              .reset().child(t("MEMCACHED", "Get", 322))
+				                              .child(e("Excetion", "NullPointException"))) //
+				      );
 
 				return t;
 			}
