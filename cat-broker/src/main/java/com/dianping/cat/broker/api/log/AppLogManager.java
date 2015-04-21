43e569ebaf4c46db539b53b4a706b41a4c87078c
youyong205 <youyong205@126.com>
2015-04-21 17:46:04 +0800

    remove log4j

--- cat-broker/src/main/java/com/dianping/cat/broker/api/log/AppLogManager.java
+++ cat-broker/src/main/java/com/dianping/cat/broker/api/log/AppLogManager.java
@@ -1,12 +1,9 @@
 package com.dianping.cat.broker.api.log;
 
-import java.net.URL;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 import org.unidal.helper.Threads;
@@ -15,15 +12,11 @@ import org.unidal.helper.Threads.Task;
 import com.dianping.cat.broker.api.app.proto.AppDataProto;
 
 public class AppLogManager implements Initializable {
-	protected Logger m_logger = Logger.getLogger(AppLogManager.class);
 
 	private BlockingQueue<AppDataProto> m_datas = new LinkedBlockingQueue<AppDataProto>(2000);
 
 	@Override
 	public void initialize() throws InitializationException {
-		URL file = this.getClass().getClassLoader().getResource("config/log4j.xml");
-		DOMConfigurator.configure(file);
-
 		Threads.forGroup("cat").start(new StoreManager());
 	}
 
@@ -45,7 +38,7 @@ public class AppLogManager implements Initializable {
 					AppDataProto proto = m_datas.poll(5, TimeUnit.MICROSECONDS);
 
 					if (proto != null) {
-						
+						// TODO save it
 					}
 				} catch (InterruptedException e) {
 					break;
