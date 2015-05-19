fc088d90fb8f97632bba187ab69b96d46997284b
youyong205 <youyong205@126.com>
2015-05-19 17:34:18 +0800

    modify the service

--- cat-core/src/main/java/com/dianping/cat/report/service/ModelService.java
+++ cat-core/src/main/java/com/dianping/cat/report/service/ModelService.java
@@ -2,8 +2,6 @@ package com.dianping.cat.report.service;
 
 public interface ModelService<M> {
 
-	public static final int ANALYZER_COUNT = 2;
-
 	public String getName();
 
 	public ModelResponse<M> invoke(ModelRequest request);
