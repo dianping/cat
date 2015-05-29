8d8432451d26aaf80757372b55b8db300e3de780
youyong205 <youyong205@126.com>
2015-05-29 20:01:03 +0800

    modify the cross

--- report-cross/src/main/java/com/dianping/cat/build/WebComponentConfigurator.java
+++ report-cross/src/main/java/com/dianping/cat/build/WebComponentConfigurator.java
@@ -3,8 +3,6 @@ package com.dianping.cat.build;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.dianping.cat.transaction.report.ReportModule;
-
 import org.unidal.lookup.configuration.Component;
 import org.unidal.web.configuration.AbstractWebComponentsConfigurator;
 
@@ -13,8 +11,6 @@ class WebComponentConfigurator extends AbstractWebComponentsConfigurator {
 	@Override
 	public List<Component> defineComponents() {
 		List<Component> all = new ArrayList<Component>();
-
-		defineModuleRegistry(all, ReportModule.class, ReportModule.class);
 
 		return all;
 	}
