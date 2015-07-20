bb709c5ff4ca59048080be41fc0c3476b38106ab
youyong205 <youyong205@126.com>
2015-07-20 10:05:20 +0800

    modify the block dumper

--- cat-consumer/src/main/java/com/dianping/cat/consumer/dump/BlockDumper.java
+++ cat-consumer/src/main/java/com/dianping/cat/consumer/dump/BlockDumper.java
@@ -2,6 +2,8 @@ package com.dianping.cat.consumer.dump;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import org.unidal.helper.Threads.Task;
@@ -20,11 +22,17 @@ public class BlockDumper implements Task {
 
 	private ServerStatisticManager m_serverStateManager;
 
+	private ThreadPoolExecutor m_executors;
+
 	public BlockDumper(ConcurrentHashMap<String, LocalMessageBucket> buckets, BlockingQueue<MessageBlock> messageBlock,
 	      ServerStatisticManager stateManager) {
+		int thread = 3;
+
 		m_buckets = buckets;
 		m_messageBlocks = messageBlock;
 		m_serverStateManager = stateManager;
+		m_executors = new ThreadPoolExecutor(thread, thread, 10, TimeUnit.SECONDS,
+		      new LinkedBlockingQueue<Runnable>(5000), new ThreadPoolExecutor.CallerRunsPolicy());
 	}
 
 	@Override
@@ -39,27 +47,55 @@ public class BlockDumper implements Task {
 				MessageBlock block = m_messageBlocks.poll(5, TimeUnit.MILLISECONDS);
 
 				if (block != null) {
-					long time = System.currentTimeMillis();
-					String dataFile = block.getDataFile();
-					LocalMessageBucket bucket = m_buckets.get(dataFile);
-
-					try {
-						bucket.getWriter().writeBlock(block);
-					} catch (Throwable e) {
-						m_errors++;
-
-						if (m_errors == 1 || m_errors % 100 == 0) {
-							Cat.logError(new RuntimeException("Error when dumping for bucket: " + dataFile + ".", e));
-						}
-					}
-					m_serverStateManager.addBlockTotal(1);
-					long duration = System.currentTimeMillis() - time;
-					m_serverStateManager.addBlockTime(duration);
+					m_executors.submit(new FlushBlockTask(block));
 				}
 			}
 		} catch (InterruptedException e) {
 			// ignore it
 		}
+	}
+
+	public class FlushBlockTask implements Task {
+
+		private MessageBlock m_block;
+
+		public FlushBlockTask(MessageBlock block) {
+			m_block = block;
+		}
+
+		@Override
+		public void run() {
+			flushBlock(m_block);
+		}
+
+		@Override
+		public String getName() {
+			return "flush-block";
+		}
+
+		@Override
+		public void shutdown() {
+		}
+
+	}
+
+	private void flushBlock(MessageBlock block) {
+		long time = System.currentTimeMillis();
+		String dataFile = block.getDataFile();
+		LocalMessageBucket bucket = m_buckets.get(dataFile);
+
+		try {
+			bucket.getWriter().writeBlock(block);
+		} catch (Throwable e) {
+			m_errors++;
+
+			if (m_errors == 1 || m_errors % 100 == 0) {
+				Cat.logError(new RuntimeException("Error when dumping for bucket: " + dataFile + ".", e));
+			}
+		}
+		m_serverStateManager.addBlockTotal(1);
+		long duration = System.currentTimeMillis() - time;
+		m_serverStateManager.addBlockTime(duration);
 	}
 
 	@Override
