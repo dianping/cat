ab9e226cb7dad4214276ceadb8181dc7cce6babf
尤勇 <yong.you@dianping.com>
2017-02-10 17:21:59 +0800

    add the aop

---
+++ 框架埋点方案集成/springAOP/cat-aop/CatAnnotation.java
@@ -0,0 +1,12 @@
+package com.dianping.cat.aop;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CatAnnotation {
+}
