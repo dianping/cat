df969572a3a0480646f7f8133942d2cddc5cd96c
yong.you <yong.you@dianping.com>
2018-06-09 13:08:34 +0800

    remove unused task

--- cat-home/src/main/java/com/dianping/cat/CatHomeModule.java
+++ cat-home/src/main/java/com/dianping/cat/CatHomeModule.java
@@ -12,21 +12,13 @@ import com.dianping.cat.analysis.MessageConsumer;
 import com.dianping.cat.analysis.TcpSocketReceiver;
 import com.dianping.cat.config.server.ServerConfigManager;
 import com.dianping.cat.consumer.CatConsumerModule;
-import com.dianping.cat.report.alert.app.AppAlert;
 import com.dianping.cat.report.alert.business.BusinessAlert;
-import com.dianping.cat.report.alert.database.DatabaseAlert;
 import com.dianping.cat.report.alert.event.EventAlert;
 import com.dianping.cat.report.alert.exception.ExceptionAlert;
-import com.dianping.cat.report.alert.exception.FrontEndExceptionAlert;
 import com.dianping.cat.report.alert.heartbeat.HeartbeatAlert;
-import com.dianping.cat.report.alert.network.NetworkAlert;
-import com.dianping.cat.report.alert.storage.StorageCacheAlert;
-import com.dianping.cat.report.alert.storage.StorageSQLAlert;
-import com.dianping.cat.report.alert.system.SystemAlert;
 import com.dianping.cat.report.alert.thirdParty.ThirdPartyAlert;
 import com.dianping.cat.report.alert.thirdParty.ThirdPartyAlertBuilder;
 import com.dianping.cat.report.alert.transaction.TransactionAlert;
-import com.dianping.cat.report.alert.web.WebAlert;
 import com.dianping.cat.report.page.ConfigReloadTask;
 import com.dianping.cat.report.task.DefaultTaskConsumer;
 
@@ -50,37 +42,40 @@ public class CatHomeModule extends AbstractModule {
 		}
 
 		if (serverConfigManager.isAlertMachine()) {
-			BusinessAlert metricAlert = ctx.lookup(BusinessAlert.class);
-			NetworkAlert networkAlert = ctx.lookup(NetworkAlert.class);
-			DatabaseAlert databaseAlert = ctx.lookup(DatabaseAlert.class);
-			SystemAlert systemAlert = ctx.lookup(SystemAlert.class);
-			ExceptionAlert exceptionAlert = ctx.lookup(ExceptionAlert.class);
-			FrontEndExceptionAlert frontEndExceptionAlert = ctx.lookup(FrontEndExceptionAlert.class);
-			HeartbeatAlert heartbeatAlert = ctx.lookup(HeartbeatAlert.class);
-			ThirdPartyAlert thirdPartyAlert = ctx.lookup(ThirdPartyAlert.class);
-			ThirdPartyAlertBuilder alertBuildingTask = ctx.lookup(ThirdPartyAlertBuilder.class);
-			AppAlert appAlert = ctx.lookup(AppAlert.class);
-			WebAlert webAlert = ctx.lookup(WebAlert.class);
 			TransactionAlert transactionAlert = ctx.lookup(TransactionAlert.class);
 			EventAlert eventAlert = ctx.lookup(EventAlert.class);
-			StorageSQLAlert storageDatabaseAlert = ctx.lookup(StorageSQLAlert.class);
-			StorageCacheAlert storageCacheAlert = ctx.lookup(StorageCacheAlert.class);
+			BusinessAlert metricAlert = ctx.lookup(BusinessAlert.class);
+			ExceptionAlert exceptionAlert = ctx.lookup(ExceptionAlert.class);
+			HeartbeatAlert heartbeatAlert = ctx.lookup(HeartbeatAlert.class);
 
-			Threads.forGroup("cat").start(networkAlert);
-			Threads.forGroup("cat").start(databaseAlert);
-			Threads.forGroup("cat").start(systemAlert);
+			ThirdPartyAlert thirdPartyAlert = ctx.lookup(ThirdPartyAlert.class);
+			ThirdPartyAlertBuilder alertBuildingTask = ctx.lookup(ThirdPartyAlertBuilder.class);
+
+			// DatabaseAlert databaseAlert = ctx.lookup(DatabaseAlert.class);
+			// SystemAlert systemAlert = ctx.lookup(SystemAlert.class);
+			// NetworkAlert networkAlert = ctx.lookup(NetworkAlert.class);
+			// FrontEndExceptionAlert frontEndExceptionAlert = ctx.lookup(FrontEndExceptionAlert.class);
+			// AppAlert appAlert = ctx.lookup(AppAlert.class);
+			// WebAlert webAlert = ctx.lookup(WebAlert.class);
+			// StorageSQLAlert storageDatabaseAlert = ctx.lookup(StorageSQLAlert.class);
+			// StorageCacheAlert storageCacheAlert = ctx.lookup(StorageCacheAlert.class);
+
 			Threads.forGroup("cat").start(metricAlert);
 			Threads.forGroup("cat").start(exceptionAlert);
-			Threads.forGroup("cat").start(frontEndExceptionAlert);
 			Threads.forGroup("cat").start(heartbeatAlert);
 			Threads.forGroup("cat").start(thirdPartyAlert);
 			Threads.forGroup("cat").start(alertBuildingTask);
-			Threads.forGroup("cat").start(appAlert);
-			Threads.forGroup("cat").start(webAlert);
 			Threads.forGroup("cat").start(transactionAlert);
 			Threads.forGroup("cat").start(eventAlert);
-			Threads.forGroup("cat").start(storageDatabaseAlert);
-			Threads.forGroup("cat").start(storageCacheAlert);
+			// Threads.forGroup("cat").start(appAlert);
+			// Threads.forGroup("cat").start(webAlert);
+			// Threads.forGroup("cat").start(storageDatabaseAlert);
+			// Threads.forGroup("cat").start(storageCacheAlert);
+			// Threads.forGroup("cat").start(frontEndExceptionAlert);
+			// Threads.forGroup("cat").start(networkAlert);
+			// Threads.forGroup("cat").start(databaseAlert);
+			// Threads.forGroup("cat").start(systemAlert);
+
 		}
 
 		final MessageConsumer consumer = ctx.lookup(MessageConsumer.class);
