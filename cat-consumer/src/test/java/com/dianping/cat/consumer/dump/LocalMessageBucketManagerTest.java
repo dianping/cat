ddfbcbf0559f0c359513a5db2634197325395215
vision <yang.zhou>
2018-07-23 18:28:38 +0800

    修复 netty在4.0.25 导致的对外内存泄露的问题 test case

--- cat-consumer/src/test/java/com/dianping/cat/consumer/dump/LocalMessageBucketManagerTest.java
+++ cat-consumer/src/test/java/com/dianping/cat/consumer/dump/LocalMessageBucketManagerTest.java
@@ -138,7 +138,8 @@ public class LocalMessageBucketManagerTest extends ComponentTestCase {
 			
 			m_codec.encode(tree, buf);
 
-			tree.setBuffer(buf);
+			byte[] bytes = new byte[buf.readableBytes()];
+			tree.setBuffer(bytes);
 			m_manager.storeMessage(tree, id);
 		}
 		
