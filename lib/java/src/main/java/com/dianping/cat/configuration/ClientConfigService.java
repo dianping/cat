5df8e3a4aee0bbdc720250087b6586a7425087c9
wangyapu <570754729@qq.com>
2018-10-09 13:52:05 +0800

    add copyright for cat client

--- lib/java/src/main/java/com/dianping/cat/configuration/ClientConfigService.java
+++ lib/java/src/main/java/com/dianping/cat/configuration/ClientConfigService.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2011-2018, Meituan Dianping. All Rights Reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.dianping.cat.configuration;
 
 import com.dianping.cat.configuration.client.entity.Server;
@@ -8,26 +26,26 @@ import java.util.List;
 
 public interface ClientConfigService {
 
-	int getClientConnectTimeout();
+    int getClientConnectTimeout();
 
-	String getDomain();
+    String getDomain();
 
-	int getLongConfigThreshold(String key);
+    int getLongConfigThreshold(String key);
 
-	int getLongThresholdByDuration(String key, int duration);
+    int getLongThresholdByDuration(String key, int duration);
 
-	String getRouters();
+    String getRouters();
 
-	double getSamplingRate();
+    double getSamplingRate();
 
-	List<Server> getServers();
+    List<Server> getServers();
 
-	boolean isMessageBlock();
+    boolean isMessageBlock();
 
-	MessageType parseMessageType(MessageTree tree);
+    MessageType parseMessageType(MessageTree tree);
 
-	void refreshConfig(PropertyConfig config);
+    void refreshConfig(PropertyConfig config);
 
-	void refreshConfig();
+    void refreshConfig();
 
 }
