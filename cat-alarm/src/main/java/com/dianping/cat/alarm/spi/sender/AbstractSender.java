a357aeb7e398d69b28ab634f83be8c927dd3f816
leon.li <leon.li@dianping.com>
2018-10-25 13:49:49 +0800

    add alarm doc

--- cat-alarm/src/main/java/com/dianping/cat/alarm/spi/sender/AbstractSender.java
+++ cat-alarm/src/main/java/com/dianping/cat/alarm/spi/sender/AbstractSender.java
@@ -18,125 +18,143 @@
  */
 package com.dianping.cat.alarm.spi.sender;
 
+import com.dianping.cat.Cat;
+import com.dianping.cat.alarm.spi.config.SenderConfigManager;
+import org.codehaus.plexus.logging.LogEnabled;
+import org.codehaus.plexus.logging.Logger;
+import org.unidal.helper.Files;
+import org.unidal.lookup.annotation.Inject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
-import org.codehaus.plexus.logging.LogEnabled;
-import org.codehaus.plexus.logging.Logger;
-import org.unidal.helper.Files;
-import org.unidal.lookup.annotation.Inject;
-
-import com.dianping.cat.Cat;
-import com.dianping.cat.alarm.spi.config.SenderConfigManager;
-
 public abstract class AbstractSender implements Sender, LogEnabled {
 
-	@Inject
-	protected SenderConfigManager m_senderConfigManager;
+    @Inject
+    protected SenderConfigManager m_senderConfigManager;
 
-	protected Logger m_logger;
+    protected Logger m_logger;
 
-	@Override
-	public void enableLogging(Logger logger) {
-		m_logger = logger;
-	}
+    @Override
+    public void enableLogging(Logger logger) {
+        m_logger = logger;
+    }
 
-	private boolean httpGetSend(String successCode, String urlPrefix, String urlPars) {
-		URL url = null;
-		InputStream in = null;
-		URLConnection conn = null;
+    private boolean httpGetSend(String successCode, String urlPrefix, String urlPars) {
+        URL url = null;
+        InputStream in = null;
+        URLConnection conn = null;
+        boolean sendSuccess = false;
 
-		try {
-			url = new URL(urlPrefix + "?" + urlPars);
-			conn = url.openConnection();
+        try {
+            url = new URL(urlPrefix + "?" + urlPars);
+            conn = url.openConnection();
 
-			conn.setConnectTimeout(2000);
-			conn.setReadTimeout(3000);
+            conn.setConnectTimeout(2000);
+            conn.setReadTimeout(3000);
 
-			in = conn.getInputStream();
-			StringBuilder sb = new StringBuilder();
-			sb.append(Files.forIO().readFrom(in, "utf-8")).append("");
+            in = conn.getInputStream();
+            StringBuilder sb = new StringBuilder();
+            sb.append(Files.forIO().readFrom(in, "utf-8")).append("");
 
-			if (sb.toString().contains(successCode)) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch (Exception e) {
-			m_logger.error(e.getMessage(), e);
-			return false;
-		} finally {
-			try {
-				if (in != null) {
-					in.close();
-				}
-			} catch (IOException e) {
-			}
-		}
-	}
+            if (sb.toString().contains(successCode)) {
+                sendSuccess = true;
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            m_logger.error(e.getMessage(), e);
+            return false;
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+            }
+            if (!sendSuccess) {
+                recordSendLog(urlPrefix, urlPars);
+            }
+        }
+    }
 
-	private boolean httpPostSend(String successCode, String urlPrefix, String content) {
-		URL url = null;
-		InputStream in = null;
-		OutputStreamWriter writer = null;
-		URLConnection conn = null;
+    private boolean httpPostSend(String successCode, String urlPrefix, String content) {
+        URL url = null;
+        InputStream in = null;
+        OutputStreamWriter writer = null;
+        URLConnection conn = null;
+        boolean sendSuccess = false;
 
-		try {
-			url = new URL(urlPrefix);
-			conn = url.openConnection();
+        try {
+            url = new URL(urlPrefix);
+            conn = url.openConnection();
 
-			conn.setConnectTimeout(2000);
-			conn.setReadTimeout(3000);
-			conn.setDoOutput(true);
-			conn.setDoInput(true);
-			conn.setRequestProperty("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
-			writer = new OutputStreamWriter(conn.getOutputStream());
+            conn.setConnectTimeout(2000);
+            conn.setReadTimeout(3000);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+            writer = new OutputStreamWriter(conn.getOutputStream());
 
-			writer.write(content);
-			writer.flush();
+            writer.write(content);
+            writer.flush();
 
-			in = conn.getInputStream();
-			StringBuilder sb = new StringBuilder();
+            in = conn.getInputStream();
+            StringBuilder sb = new StringBuilder();
 
-			sb.append(Files.forIO().readFrom(in, "utf-8")).append("");
-			if (sb.toString().contains(successCode)) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch (Exception e) {
-			m_logger.error(e.getMessage(), e);
-			return false;
-		} finally {
-			try {
-				if (in != null) {
-					in.close();
-				}
-				if (writer != null) {
-					writer.close();
-				}
-			} catch (IOException e) {
-			}
-		}
-	}
+            sb.append(Files.forIO().readFrom(in, "utf-8")).append("");
+            if (sb.toString().contains(successCode)) {
+                sendSuccess = true;
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            m_logger.error(e.getMessage(), e);
+            return false;
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+            }
+            if (!sendSuccess) {
+                recordSendLog(urlPrefix, content);
+            }
+        }
+    }
 
-	public boolean httpSend(String successCode, String type, String urlPrefix, String urlPars) {
-		if ("get".equalsIgnoreCase(type)) {
-			return httpGetSend(successCode, urlPrefix, urlPars);
-		} else if ("post".equalsIgnoreCase(type)) {
-			return httpPostSend(successCode, urlPrefix, urlPars);
-		} else {
-			Cat.logError(new RuntimeException("Illegal request type: " + type));
-			return false;
-		}
-	}
+    public boolean httpSend(String successCode, String type, String urlPrefix, String urlPars) {
+        if ("get".equalsIgnoreCase(type)) {
+            return httpGetSend(successCode, urlPrefix, urlPars);
+        } else if ("post".equalsIgnoreCase(type)) {
+            return httpPostSend(successCode, urlPrefix, urlPars);
+        } else {
+            Cat.logError(new RuntimeException("Illegal request type: " + type));
+            return false;
+        }
+    }
 
-	public com.dianping.cat.alarm.sender.entity.Sender querySender() {
-		String id = getId();
+    public com.dianping.cat.alarm.sender.entity.Sender querySender() {
+        String id = getId();
 
-		return m_senderConfigManager.querySender(id);
-	}
+        return m_senderConfigManager.querySender(id);
+    }
+
+    private void recordSendLog(String urlPrefix, String paras) {
+        Cat.logError(urlPrefix + "---" + paras, new AlertSendException());
+    }
+
+    private class AlertSendException extends Exception {
+
+    }
+
 }
