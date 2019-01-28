6199dd55e0d8b1d61f08f0a9ebe9281b9f6a6e2a
stdrickforce <stdrickforce@gmail.com>
2019-01-28 15:29:12 +0800

    ccat 3.0.1

--- lib/c/src/lib/cat_network_util.h
+++ lib/c/src/lib/cat_network_util.h
@@ -19,15 +19,8 @@
 #ifndef CAT_CLIENT_C_NETWORK_UTIL_H
 #define CAT_CLIENT_C_NETWORK_UTIL_H
 
-#include "cat_sds.h"
-
 int getLocalHostIp(char *ipBuf);
 
-int getLocalHostName(char *host, size_t bufLen);
-
-int hostnameToIp(char *hostname, char *ip);
-
 int getLocalHostIpHex(char *ipHexBuf);
-
 
 #endif //CAT_CLIENT_C_NETWORK_UTIL_H
