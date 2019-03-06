3666278e277f17231c3009e0e20d0100263aaf8b
wangyapu <wangyapu0714@gmail.com>
2019-03-06 11:32:03 +0800

    modify dockerfile

--- docker/datasources.sh
+++ docker/datasources.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sed -i "s/MYSQL_URL/${MYSQL_URL}/g" /data/appdatas/cat/datasources.xml;
 sed -i "s/MYSQL_PORT/${MYSQL_PORT}/g" /data/appdatas/cat/datasources.xml;
 sed -i "s/MYSQL_USERNAME/${MYSQL_USERNAME}/g" /data/appdatas/cat/datasources.xml;
