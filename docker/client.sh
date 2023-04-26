39ad1a56a90e9ea5be3bc1d474177e8b0857a24c
guoyuanlu <guoyuanlu@puyiwm.com>
2023-04-26 13:06:16 +0800

    update once

--- docker/client.sh
+++ docker/client.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "initialize client.xml"
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><config mode=\"client\"><servers><server ip=\"${SERVER_URL}\" port=\"2280\" http-port=\"8080\"/></servers></config>" > /data/appdatas/cat/client.xml
-#echo "$(sed 's/SERVER_URL/${SERVER_URL}/g' /data/appdatas/cat/client.xml)" > /data/appdatas/cat/client.xml
+sed -i "s/SERVER_URL/${SERVER_URL}/g" /data/appdatas/cat/client.xml;
--- docker/datasources.sh
+++ docker/datasources.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "initialize datasources.xml"
-echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><data-sources><data-source id=\"cat\"><maximum-pool-size>3</maximum-pool-size><connection-timeout>1s</connection-timeout><idle-timeout>10m</idle-timeout><statement-cache-size>1000</statement-cache-size><properties><driver>com.mysql.jdbc.Driver</driver><url><![CDATA[jdbc:mysql://${MYSQL_URL}:${MYSQL_PORT}/${MYSQL_SCHEMA}]]></url><user>${MYSQL_USERNAME}</user><password>${MYSQL_PASSWD}</password><connectionProperties><![CDATA[useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&socketTimeout=120000]]></connectionProperties></properties></data-source></data-sources>" > /data/appdatas/cat/datasources.xml;
-#echo "$(sed 's/MYSQL_URL/${MYSQL_URL}/g' /data/appdatas/cat/datasources.xml)" > /data/appdatas/cat/datasources.xml;
-#echo "$(sed 's/MYSQL_PORT/${MYSQL_PORT}/g' /data/appdatas/cat/datasources.xml)" > /data/appdatas/cat/datasources.xml;
-#echo "$(sed 's/MYSQL_USERNAME/${MYSQL_USERNAME}/g' /data/appdatas/cat/datasources.xml)" > /data/appdatas/cat/datasources.xml;
-#echo "$(sed 's/MYSQL_PASSWD/${MYSQL_PASSWD}/g' /data/appdatas/cat/datasources.xml)" > /data/appdatas/cat/datasources.xml;
-#echo "$(sed 's/MYSQL_SCHEMA/${MYSQL_SCHEMA}/g' /data/appdatas/cat/datasources.xml)" > /data/appdatas/cat/datasources.xml;
+sed -i "s/MYSQL_URL/${MYSQL_URL}/g" /data/appdatas/cat/datasources.xml;
+sed -i "s/MYSQL_PORT/${MYSQL_PORT}/g" /data/appdatas/cat/datasources.xml;
+sed -i "s/MYSQL_USERNAME/${MYSQL_USERNAME}/g" /data/appdatas/cat/datasources.xml;
+sed -i "s/MYSQL_PASSWD/${MYSQL_PASSWD}/g" /data/appdatas/cat/datasources.xml;
+sed -i "s/MYSQL_SCHEMA/${MYSQL_SCHEMA}/g" /data/appdatas/cat/datasources.xml;
