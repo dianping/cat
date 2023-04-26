2aa09b0fccb690d7754d1bd57500af425460ce2b
guoyuanlu <guoyuanlu@puyiwm.com>
2023-04-26 10:33:39 +0800

    update

--- docker/client.sh
+++ docker/client.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "initialize client.xml"
 echo "$(sed 's/SERVER_URL/${SERVER_URL}/g' /data/appdatas/cat/client.xml)" > /data/appdatas/cat/client.xml
--- docker/datasources.sh
+++ docker/datasources.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "initialize datasources.xml"
 echo "$(sed 's/MYSQL_URL/${MYSQL_URL}/g' /data/appdatas/cat/datasources.xml)" > /data/appdatas/cat/datasources.xml;
 echo "$(sed 's/MYSQL_PORT/${MYSQL_PORT}/g' /data/appdatas/cat/datasources.xml)" > /data/appdatas/cat/datasources.xml;
 echo "$(sed 's/MYSQL_USERNAME/${MYSQL_USERNAME}/g' /data/appdatas/cat/datasources.xml)" > /data/appdatas/cat/datasources.xml;
