479a0427284caaaf96be4eca119ec317cba65c39
guoyuanlu <guoyuanlu@puyiwm.com>
2023-04-26 10:49:13 +0800

    update once

--- docker/client.sh
+++ docker/client.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 echo "initialize client.xml"
-echo "$(sed 's/SERVER_URL/${SERVER_URL}/g' /data/appdatas/cat/client.xml)" > /data/appdatas/cat/client.xml
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><config mode=\"client\"><servers><server ip=\"${SERVER_URL}\" port=\"2280\" http-port=\"8080\"/></servers></config>" > /data/appdatas/cat/client.xml
+#echo "$(sed 's/SERVER_URL/${SERVER_URL}/g' /data/appdatas/cat/client.xml)" > /data/appdatas/cat/client.xml
