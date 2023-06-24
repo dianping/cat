c6381bef6d233f8c3db7570e4bcb900600db6751
shiyindaxiaojie <1813986321@qq.com>
2023-06-24 23:50:29 +0800

    update

--- docs/scripts/cat-init-3.3.0.sql
+++ docs/scripts/cat-init-3.3.0.sql
@@ -303,22 +303,6 @@ CREATE TABLE `server_alarm_rule` (
       KEY `updatetime` (`updatetime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统告警的配置';
 
-/*
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 CREATE TABLE `ajax_data_0`
 (
 	`id`                int(11)     NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
@@ -386,6 +370,89 @@ CREATE TABLE `js_error_log_content`
 	KEY `updatetime` (`updatetime`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
+
+CREATE TABLE `web_speed_data_0`
+(
+	`id`                  int(11)     NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
+	`period`              date        NOT NULL COMMENT '时间',
+	`minute_order`        smallint(6) NOT NULL COMMENT '分钟',
+	`city`                smallint(6) NOT NULL COMMENT '城市',
+	`platform`            smallint(6) NOT NULL COMMENT '平台',
+	`operator`            smallint(6) NOT NULL COMMENT '运营商',
+	`network`             smallint(6) NOT NULL COMMENT '网络类型',
+	`source`              smallint(6) NOT NULL COMMENT '来源',
+	`access_number1`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number2`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number3`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number4`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number5`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number6`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number7`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number8`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number9`      bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number10`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number11`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number12`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number13`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number14`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number15`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number16`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number17`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number18`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number19`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number20`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number21`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number22`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number23`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number24`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number25`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number26`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number27`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number28`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number29`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number30`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number31`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`access_number32`     bigint(20) DEFAULT '0' COMMENT '访问量',
+	`response_sum_time1`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time2`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time3`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time4`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time5`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time6`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time7`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time8`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time9`  bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time10` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time11` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time12` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time13` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time14` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time15` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time16` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time17` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time18` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time19` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time20` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time21` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time22` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time23` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time24` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time25` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time26` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time27` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time28` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time29` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time30` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time31` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`response_sum_time32` bigint(20) DEFAULT '0' COMMENT '响应时间大小',
+	`status`              smallint(6) NOT NULL COMMENT '数据状态',
+	`updatetime`          datetime    NOT NULL COMMENT '数据更新时间',
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `IX_condition` (`period`, `minute_order`, `city`, `operator`, `network`, `platform`, `source`),
+	KEY `updatetime` (`updatetime`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 227
+  DEFAULT CHARSET = utf8 COMMENT ='web测速数据';
 
 CREATE TABLE `web_speed_data_1`
 (
@@ -470,6 +537,30 @@ CREATE TABLE `web_speed_data_1`
   AUTO_INCREMENT = 227
   DEFAULT CHARSET = utf8 COMMENT ='web测速数据';
 
+CREATE TABLE `app_command_data_0`
+(
+	`id`                int(11)  NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
+	`period`            date     NOT NULL COMMENT '时间',
+	`minute_order`      smallint NOT NULL COMMENT '分钟',
+	`city`              smallint NOT NULL COMMENT '城市',
+	`operator`          tinyint  NOT NULL COMMENT '运营商',
+	`network`           tinyint  NOT NULL COMMENT '网络类型',
+	`app_version`       int      NOT NULL COMMENT '版本',
+	`connect_type`      tinyint  NOT NULL COMMENT '访问类型，是否长连接',
+	`code`              smallint NOT NULL COMMENT '返回码',
+	`platform`          tinyint  NOT NULL COMMENT '平台',
+	`access_number`     bigint   NOT NULL COMMENT '访问量',
+	`response_sum_time` bigint   NOT NULL COMMENT '响应时间大小',
+	`request_package`   bigint   NOT NULL COMMENT '请求包大小',
+	`response_package`  bigint   NOT NULL COMMENT '响应包大小',
+	`status`            smallint NOT NULL COMMENT '数据状态',
+	`creation_date`     datetime NOT NULL COMMENT '数据插入时间',
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `IX_condition` (`period`, `minute_order`, `city`, `operator`, `network`, `app_version`, `connect_type`,
+							   `code`, `platform`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='app基本数据';
+
 CREATE TABLE `app_command_data_1`
 (
 	`id`                int(11)  NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
@@ -497,6 +588,27 @@ CREATE TABLE `app_command_data_1`
 CREATE table app_command_data_2 like app_command_data_1;
 CREATE table app_command_data_3 like app_command_data_1;
 CREATE table app_command_data_4 like app_command_data_1;
+
+CREATE TABLE `app_speed_data_0`
+(
+	`id`                     int(11)  NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
+	`period`                 date     NOT NULL COMMENT '时间',
+	`minute_order`           smallint NOT NULL COMMENT '分钟',
+	`city`                   smallint NOT NULL COMMENT '城市',
+	`operator`               tinyint  NOT NULL COMMENT '运营商',
+	`network`                tinyint  NOT NULL COMMENT '网络类型',
+	`app_version`            int      NOT NULL COMMENT '版本',
+	`platform`               tinyint  NOT NULL COMMENT '平台',
+	`access_number`          bigint   NOT NULL COMMENT '访问量',
+	`slow_access_number`     bigint   NOT NULL COMMENT '慢用户访问量',
+	`response_sum_time`      bigint   NOT NULL COMMENT '响应时间大小',
+	`slow_response_sum_time` bigint   NOT NULL COMMENT '慢用户响应时间大小',
+	`status`                 smallint NOT NULL COMMENT '数据状态',
+	`creation_date`          datetime NOT NULL COMMENT '数据插入时间',
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `IX_condition` (period, minute_order, city, operator, network, app_version, platform)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='app测速数据';
 
 CREATE TABLE `app_speed_data_1`
 (
